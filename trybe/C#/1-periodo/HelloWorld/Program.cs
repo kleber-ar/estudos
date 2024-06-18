852e80976658c95aa2d294c77848e8969403253b
kleber-ar <kleber-ar@hotmail.com>
2024-06-18 18:11:11 -0300

    C#

---
+++ trybe/C#/1-periodo/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Greeting
+{
+    public static string Greet()
+    {
+        return "Olá, Mundo!";
+    }
+
+    public static string GreetTryber()
+    {
+        return "Seja bem-vindo, Tryber!";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Greeting.Greet());
+        Console.WriteLine(Greeting.GreetTryber());
+    }
+}
