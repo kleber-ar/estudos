f7c10590e60f47794c6c7c5d2cd551916399d98e
kleber-ar <kleber-ar@hotmail.com>
2025-06-17 12:39:50 -0300

    blogger att2

--- trybe/front-end/1-periodo/secao-3/sd-034-exercise-bloggers/src/pages/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Users } from './Users';
-export { default as Posts } from './Posts';
-export { default as About } from './About';
-export { default as NotFound } from './NotFound';
