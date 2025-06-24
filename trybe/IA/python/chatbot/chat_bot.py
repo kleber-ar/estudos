2cc1fc9e0ebc0e450d0a3b18c5c6a4c17a11edd5
kleber-ar <kleber-ar@hotmail.com>
2025-06-24 16:36:03 -0300

    AI - ler um arquivo usando o upload

--- trybe/IA/python/chatbot/chat_bot.py
+++ trybe/IA/python/chatbot/chat_bot.py
@@ -10,14 +10,19 @@ load_dotenv()
 # Inicialização do cliente (agora a variável GEMINI_API_KEY será carregada)
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-# ler um arquivo
-with open("curriculo-joao-silva.txt", "r") as file:
-    curriculum = file.read();
+# ler um arquivo.txt--------------
+
+#with open("curriculo-joao-silva.txt", "r") as file:
+#    curriculum = file.read();
+
+#--------------------------------
+
+students_notes = client.files.upload(file="desempenho_estudantes_enem.csv")
 
 # Chamada direta usando o cliente
 response = client.models.generate_content(
     model='gemini-2.5-flash', # ou outro modelo como 'gemini-2.0-flash'
-    contents=f'analise esse {curriculum}',
+    contents=[f'gere um relatorio:',students_notes]
 )
 
 print(response.text)
