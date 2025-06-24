ba99128eaff9dfec268bce612dabee8b1078efd1
kleber-ar <kleber-ar@hotmail.com>
2025-06-24 16:01:55 -0300

    ler um arquivo

--- trybe/IA/python/chatbot/chat_bot.py
+++ trybe/IA/python/chatbot/chat_bot.py
@@ -10,10 +10,14 @@ load_dotenv()
 # Inicialização do cliente (agora a variável GEMINI_API_KEY será carregada)
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
+# ler um arquivo
+with open("curriculo-joao-silva.txt", "r") as file:
+    curriculum = file.read();
+
 # Chamada direta usando o cliente
 response = client.models.generate_content(
     model='gemini-2.5-flash', # ou outro modelo como 'gemini-2.0-flash'
-    contents='Escreva uma piada.'
+    contents=f'analise esse {curriculum}',
 )
 
 print(response.text)
