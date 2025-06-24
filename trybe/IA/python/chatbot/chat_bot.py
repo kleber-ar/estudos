cdb3ec32d06b5a2e64ce583fe45deaa7537d2b9d
kleber-ar <kleber-ar@hotmail.com>
2025-06-24 17:04:52 -0300

    AI - ler um arquivos(qualquer pelo jeito) usando o upload, e textos baseados em imagens

--- trybe/IA/python/chatbot/chat_bot.py
+++ trybe/IA/python/chatbot/chat_bot.py
@@ -19,12 +19,13 @@ client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 #ler qualquer arquivo
 
-prato= client.files.upload(file="prato-de-comida.png")
+foto1= client.files.upload(file="social_media_festa.png")
+foto2= client.files.upload(file="social_media_viagem.png")
 
 # Chamada direta usando o cliente
 response = client.models.generate_content(
     model='gemini-2.0-flash', # ou outro modelo como 'gemini-2.0-flash'
-    contents=[f'estime as calorias:',prato]
+    contents=[f'faça um convide curto baseado na foto:',foto2]
 )
 
 print(response.text)
