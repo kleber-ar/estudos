5e644fb2e25ce4f9cd912ac6fc70d33cf1814f03
kleber-ar <kleber-ar@hotmail.com>
2025-06-24 18:57:57 -0300

    AI - gradio funcionando

--- trybe/IA/python/chatbot_gradio/README.md
+++ trybe/IA/python/chatbot_gradio/README.md
@@ -2,4 +2,7 @@ A API_KEY ta dentro de um .env e não dentro do linux num export.
 
 O pyrightconfig.json é necessário para parar de da erro no import, caso use NVIM.
 
-installe usando: ➜ pip install google-genai ➜ pip install python-dotenv (pro .env funfa)
+installe usando: 
+➜ pip install google-genai 
+➜ pip install python-dotenv (pro .env funfa)
+➜ pip install gradio
--- trybe/IA/python/chatbot_gradio/chatbot_gradio.py
+++ trybe/IA/python/chatbot_gradio/chatbot_gradio.py
@@ -4,15 +4,20 @@
 from google import genai
 import os
 from dotenv import load_dotenv # Adicione esta importação
+import gradio
 
 # Carrega as variáveis do arquivo .env
 load_dotenv()
-
 # Inicialização do cliente (agora a variável GEMINI_API_KEY será carregada)
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 # Chamada para chat
 chat = client.chats.create(model='gemini-2.0-flash')
-response = chat.send_message('oi')
+chat.send_message('voce é um consultor de programação')
 
-print(response.text)
+def gradio_wrapper(message, _history):
+    response = chat.send_message(message)
+    return response.text
+
+chatInterface = gradio.ChatInterface(gradio_wrapper)
+chatInterface.launch()
