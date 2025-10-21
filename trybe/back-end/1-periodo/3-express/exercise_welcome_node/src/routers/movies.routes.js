3fb143f780c5dea42742e8ea0dca9f10f4d20c7d
kleber-ar <kleber-ar@hotmail.com>
2025-10-21 18:09:00 -0300

    end point POST

--- trybe/back-end/1-periodo/3-express/exercise_welcome_node/src/routers/movies.routes.js
+++ trybe/back-end/1-periodo/3-express/exercise_welcome_node/src/routers/movies.routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
+
 const readJsonData = require('../utils/fs/readJsonData');
+const writeJsonData = require('../utils/fs/writeJsonData');
+const findNextId = require('../utils/findNextId');
 
 const router = express.Router();
 
@@ -21,6 +24,18 @@ router.get('/movies', async (req, res) => {
   const content = await readJsonData(PATH);
 
   res.status(200).json(content);
+});
+
+router.post('/movies', async (req, res) => {
+  const content = await readJsonData(PATH);
+  const movieContent = req.body;
+  const nextId = findNextId(content);
+  const newMovie = { id: nextId, ...movieContent };
+  const newContent = [...content, newMovie];
+
+  await writeJsonData(PATH, newContent);
+
+  res.status(201).json(newMovie);
 });
 
 module.exports = router;
