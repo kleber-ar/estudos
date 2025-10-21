2de83cd4773f5105ae59d8dee3a9a05efaec1e60
kleber-ar <kleber-ar@hotmail.com>
2025-10-21 16:48:43 -0300

    end point do ID feito e o middleware

--- trybe/back-end/1-periodo/3-express/exercise_welcome_node/src/app.js
+++ trybe/back-end/1-periodo/3-express/exercise_welcome_node/src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const moviesRouter = require('./routers/movies.routes');
 
 const app = express();
 app.use(express.json());
+
+app.use('/movies', moviesRouter);
 
 module.exports = app;
--- trybe/back-end/1-periodo/3-express/exercise_welcome_node/src/routers/movies.routes.js
+++ trybe/back-end/1-periodo/3-express/exercise_welcome_node/src/routers/movies.routes.js
@@ -1,1 +1,19 @@
+const express = require('express');
+const path = require('path');
+const readJsonData = require('../utils/fs/readJsonData');
 
+const router = express.Router();
+
+const PATH = path.resolve('src', 'movies.json');
+
+router.get('/movies/:id', async (req, res) => {
+  const { id } = req.params;
+  const content = await readJsonData(PATH);
+  const movieFound = content.find((movie) => movie.id === id);
+
+  if (!movieFound) return res.status(404).json({ message: 'Filme não econtrado' });
+
+  res.status(200).json(movieFound);
+});
+
+module.exports = router;
