0a4ad64d4c861fdb48e000e928bd5938a3db8ae1
kleber-ar <kleber-ar@hotmail.com>
2025-10-21 16:33:38 -0300

    função para ler arquivo json

--- trybe/back-end/1-periodo/3-express/exercise_welcome_node/src/utils/fs/readJsonData.js
+++ trybe/back-end/1-periodo/3-express/exercise_welcome_node/src/utils/fs/readJsonData.js
@@ -1,1 +1,9 @@
+const fs = require('fs/promises');
 
+const readJsonData = async (path) => {
+  const content = await fs.readFile(path, 'utf-8');
+
+  return JSON.parse(content);
+};
+
+module.exports = readJsonData;
