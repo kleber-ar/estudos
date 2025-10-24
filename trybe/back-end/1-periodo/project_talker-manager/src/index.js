d466626d1e08b66f5a51eb70434e6a353b467975
kleber-ar <kleber-ar@hotmail.com>
2025-10-24 20:53:34 -0300

    usando errors globais

--- trybe/back-end/1-periodo/project_talker-manager/src/index.js
+++ trybe/back-end/1-periodo/project_talker-manager/src/index.js
@@ -12,8 +12,16 @@ app.listen(PORT, () => {
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
+  response.status(200).send();
 });
 
 app.use('/', routes);
 
+// Middleware para rotas não encontradas (404)
+app.use((req, res) => res.sendStatus(404));
+
+// Middleware de tratamento de erros (500)
+app.use((err, req, res, next) => {
+  console.error(err); // loga o erro no console
+  res.status(500).json({ message: 'Erro interno no servidor' });
+});
