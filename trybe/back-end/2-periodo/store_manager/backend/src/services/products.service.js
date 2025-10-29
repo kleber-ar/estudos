e56c0f3335ac4501d5858b9cea0a70434b0290b2
kleber-ar <kleber-ar@hotmail.com>
2025-10-29 12:35:50 -0300

    create products controller list... and get...

--- trybe/back-end/2-periodo/store_manager/backend/src/services/products.service.js
+++ trybe/back-end/2-periodo/store_manager/backend/src/services/products.service.js
@@ -2,13 +2,13 @@ const productsModel = require('../models/products.model');
 
 async function getAll() {
   const products = await productsModel.findAll();
-  return products;
+  return { status: 'SUCCESSFUL', products };
 }
 
 async function getById(id) {
   const product = await productsModel.findById(id);
-  if (!product) return { type: 'NOT_FOUND', message: 'Product not found' };
-  return product;
+  if (!product) return { status: 'NOT_FOUND', data: { message: 'Product not found' } };
+  return { status: 'SUCCESSFUL', data: product };
 }
 
 module.exports = { getAll, getById };
