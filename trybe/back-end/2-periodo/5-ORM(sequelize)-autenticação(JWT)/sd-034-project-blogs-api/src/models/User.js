455260f06177da46305bcb06a1053c92667ed81a
kleber-ar <kleber-ar@hotmail.com>
2025-11-06 08:05:59 -0300

    create models User

---
+++ trybe/back-end/2-periodo/5-ORM(sequelize)-autenticação(JWT)/sd-034-project-blogs-api/src/models/User.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      displayName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: 'display_name',
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    {
+      tableName: 'users',
+      underscored: true,
+      timestamps: false,
+    },
+  );
+
+  return User;
+};
