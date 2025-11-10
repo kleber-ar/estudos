0f945431eba9af503830059ce27c3a7aa72881f0
kleber-ar <kleber-ar@hotmail.com>
2025-11-10 08:45:05 -0300

    typeScript - mercadin do Sr.Scott

---
+++ trybe/back-end/3-periodo/7-seção_typeScript/exercise-mercadinho_do_sr-scott/src/functions.ts
@@ -0,0 +1,17 @@
+export function first<Type>(items: Type[]): Type {
+  return items[0];
+}
+
+export function updateItem<Type>(items: Type[], index: number, updatedItem: Type): Type[] {
+  if (index >= 0 && index < items.length) items.splice(index, 1, updatedItem);
+  return items;
+}
+
+import { Food } from './types/Food';
+import { Drink } from './types/Drink';
+import { CartItem } from './types/CartItem';
+
+export function buildCartItem({ name, price }: Food | Drink, quantity: number): CartItem {
+  return { name, price, quantity } as CartItem;
+}
+
