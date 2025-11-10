90dba8600f95bf1de0e22190c0c6548605fc8e82
kleber-ar <kleber-ar@hotmail.com>
2025-11-10 12:24:55 -0300

    add validations and statushttp

--- trybe/back-end/3-periodo/7-seção_typeScript/transactions-api/src/controllers/transactions.controller.ts
+++ trybe/back-end/3-periodo/7-seção_typeScript/transactions-api/src/controllers/transactions.controller.ts
@@ -1,12 +1,18 @@
 import { Request, Response } from 'express';
 import transactionsService from '../services/transactions.service';
+import statusHTTP from '../utils/statusHTTP';
 
 async function create(req: Request, res: Response) {
   const { name, price, type, userId } = req.body;
-  const transaction = await transactionsService.create({ name, price, type, userId });
-  res.status(201).json(transaction);
-}
 
+  const serviceResponse = await transactionsService.create({ name, price, type, userId });
+
+  if (serviceResponse.status !== 'SUCCESSFUL') {
+    return res.status(statusHTTP(serviceResponse.status)).json(serviceResponse.data);
+  }
+
+  res.status(201).json(serviceResponse.data);
+}
 export default {
   create,
 };
--- trybe/back-end/3-periodo/7-seção_typeScript/transactions-api/src/services/transactions.service.ts
+++ trybe/back-end/3-periodo/7-seção_typeScript/transactions-api/src/services/transactions.service.ts
@@ -1,11 +1,36 @@
 import TransactionModel, {
   TransactionInputtableFields,
 } from '../database/models/transaction.model';
+import { ServiceResponse } from '../types/ServiceResponse';
+import { Transaction } from '../types/Transaction';
 
-async function create(transaction: TransactionInputtableFields) {
+function validateParams({
+  name,
+  price,
+  type,
+  userId,
+}: TransactionInputtableFields): string | null {
+  if (!name) return 'Name is required';
+  if (!price) return 'Price is required';
+  if (!type) return 'Type is required';
+  if (!userId) return 'userId is required';
+
+  return null;
+}
+
+async function create(transaction: TransactionInputtableFields): Promise<ServiceResponse<Transaction>> {
+  let responseService: ServiceResponse<Transaction>;
+  const error = validateParams(transaction);
+
+  if (error) {
+    responseService = { status: 'INVALID_DATA', data: { message: error } };
+    return responseService;
+  }
   const newTransaction = await TransactionModel.create(transaction);
 
-  return newTransaction.dataValues;
+  responseService = { status: 'SUCCESSFUL', data: newTransaction.dataValues };
+
+  return responseService;
 }
 
 export default {
