8bb382e5d3102d47a8a166064ccecd93fde1ee41
kleber-ar <kleber-ar@hotmail.com>
2025-11-10 14:52:27 -0300

    add login e route and jwt middlewares

--- trybe/back-end/3-periodo/7-seção_typeScript/transactions-api/src/app.ts
+++ trybe/back-end/3-periodo/7-seção_typeScript/transactions-api/src/app.ts
@@ -1,4 +1,6 @@
 import express, { Request, Response } from 'express';
+import authMiddleware from './middlewares/auth';
+import loginRouter from './routers/login.router';
 import transactionsRouter from './routers/transactions.router';
 
 const app = express();
@@ -9,6 +11,8 @@ app.get('/', (_req: Request, res: Response) => {
   res.status(200).send('Aplicação está funcionando!');
 });
 
+app.use(loginRouter);
+app.use(authMiddleware);
 app.use(transactionsRouter);
 
 export default app;
