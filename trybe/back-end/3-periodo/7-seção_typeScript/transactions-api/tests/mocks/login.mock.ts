ad3a1e17ea6bfc3d0fa92cb8e753e6f22ab9b360
kleber-ar <kleber-ar@hotmail.com>
2025-11-12 12:18:10 -0300

    add testes POST login and mocks

---
+++ trybe/back-end/3-periodo/7-seção_typeScript/transactions-api/tests/mocks/login.mock.ts
@@ -0,0 +1,25 @@
+const validEmail = 'user1@email.com';
+const noPasswordLoginBody = { email: validEmail, password: '' };
+
+const validPassword = 'ch4ng3m3';
+const noEmailLoginBody = { email: '', password: validPassword };
+
+const notExistingUserBody = { email: 'notfound@email.com', password: validPassword };
+const existingUserWithWrongPasswordBody = { email: validEmail, password: 'wrong_password' };
+const existingUser = {
+  id: 1,
+  email: validEmail,
+  password: validPassword,
+  name: 'user1'
+};
+
+const validLoginBody = { email: validEmail, password: validPassword };
+
+export default {
+  noEmailLoginBody,
+  noPasswordLoginBody,
+  notExistingUserBody,
+  existingUserWithWrongPasswordBody,
+  existingUser,
+  validLoginBody,
+};
