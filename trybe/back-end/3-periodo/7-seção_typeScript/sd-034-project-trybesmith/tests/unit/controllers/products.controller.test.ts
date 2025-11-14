a74b905171e9e310d5344c3ba0f9d358a570d65b
kleber-ar <kleber-ar@hotmail.com>
2025-11-14 18:19:46 -0300

    add test unit getAll products service,controller

--- trybe/back-end/3-periodo/7-seção_typeScript/sd-034-project-trybesmith/tests/unit/controllers/products.controller.test.ts
+++ trybe/back-end/3-periodo/7-seção_typeScript/sd-034-project-trybesmith/tests/unit/controllers/products.controller.test.ts
@@ -11,62 +11,92 @@ describe('Controller - Products', function () {
     sinon.restore();
   });
 
-  it('retorna 201 e o produto criado', async function () {
-    const req = {
-      body: {
-        name: 'Martelo de Thor',
-        price: '30 peças de ouro',
-        userId: 1,
-      },
-    } as any;
+  describe('Create products POST', function () {
+    it('retorna 201 e o produto criado', async function () {
+      const req = {
+        body: {
+          name: 'Martelo de Thor',
+          price: '30 peças de ouro',
+          userId: 1,
+        },
+      } as any;
 
-    const res = {
-      status: sinon.stub().returnsThis(),
-      json: sinon.stub(),
-    } as any;
+      const res = {
+        status: sinon.stub().returnsThis(),
+        json: sinon.stub(),
+      } as any;
 
-    const mockServiceResponse: ServiceResponse<Product> = {
-      status: 'CREATED',
-      data: {
-        id: 6,
-        name: 'Martelo de Thor',
-        price: '30 peças de ouro',
-        userId: 1,
-      },
-    };
+      const mockServiceResponse: ServiceResponse<Product> = {
+        status: 'CREATED',
+        data: {
+          id: 6,
+          name: 'Martelo de Thor',
+          price: '30 peças de ouro',
+          userId: 1,
+        },
+      };
 
-    sinon.stub(productsService, 'create').resolves(mockServiceResponse);
+      sinon.stub(productsService, 'create').resolves(mockServiceResponse);
 
-    await productsController.create(req, res);
+      await productsController.create(req, res);
 
-    expect(res.status.calledWith(statusHTTP('CREATED'))).to.be.true;
-    expect(res.json.calledWith(mockServiceResponse.data)).to.be.true;
+      expect(res.status.calledWith(statusHTTP('CREATED'))).to.be.true;
+      expect(res.json.calledWith(mockServiceResponse.data)).to.be.true;
+    });
+
+    it('retorna o status e mensagem correta quando o status NÃO é CREATED', async function () {
+      const req = {
+        body: {
+          name: 'Produto X',   // TEM QUE SER VALORES VÁLIDOS
+          price: '100',
+          userId: 99,
+        },
+      } as any;
+
+      const res = {
+        status: sinon.stub().returnsThis(),
+        json: sinon.stub(),
+      } as any;
+
+      const mockErrorResponse: ServiceResponse<Product> = {
+        status: 'BAD_REQUEST',
+        data: { message: 'Dados inválidos' },
+      };
+
+      sinon.stub(productsService, 'create').resolves(mockErrorResponse);
+
+      await productsController.create(req, res);
+
+      expect(res.status.calledWith(statusHTTP('BAD_REQUEST'))).to.be.true;
+      expect(res.json.calledWith({ message: 'Dados inválidos' })).to.be.true;
+    });
+  })
+
+  describe('Get all products GET', function () {
+
+    it('retorna 200 e a lista de produtos', async function () {
+      const req = {} as any;
+      const res = {
+        status: sinon.stub().returnsThis(),
+        json: sinon.stub(),
+      } as any;
+
+      const mockResponse: ServiceResponse<Product[]> = {
+        status: 'SUCCESSFUL',
+        data: [
+          { id: 1, name: 'Excalibur', price: '10 peças de ouro', userId: 1 },
+          { id: 2, name: 'Espada Justiceira', price: '20 peças de ouro', userId: 1 },
+        ],
+      };
+
+      sinon.stub(productsService, 'getAll').resolves(mockResponse);
+
+      await productsController.getAll(req, res);
+
+      expect(res.status.calledWith(200)).to.be.true;
+      expect(res.json.calledWith(mockResponse.data)).to.be.true;
+    });
   });
 
-  it('retorna o status e mensagem correta quando o status NÃO é CREATED', async function () {
-    const req = {
-      body: {
-        name: 'Produto X',   // TEM QUE SER VALORES VÁLIDOS
-        price: '100',
-        userId: 99,
-      },
-    } as any;
 
-    const res = {
-      status: sinon.stub().returnsThis(),
-      json: sinon.stub(),
-    } as any;
-
-    const mockErrorResponse: ServiceResponse<Product> = {
-      status: 'BAD_REQUEST',
-      data: { message: 'Dados inválidos' },
-    };
-
-    sinon.stub(productsService, 'create').resolves(mockErrorResponse);
-
-    await productsController.create(req, res);
-
-    expect(res.status.calledWith(statusHTTP('BAD_REQUEST'))).to.be.true;
-    expect(res.json.calledWith({ message: 'Dados inválidos' })).to.be.true;
-  });
 });
--- trybe/back-end/3-periodo/7-seção_typeScript/sd-034-project-trybesmith/tests/unit/services/products.service.test.ts
+++ trybe/back-end/3-periodo/7-seção_typeScript/sd-034-project-trybesmith/tests/unit/services/products.service.test.ts
@@ -9,30 +9,55 @@ describe('Service - Products', function () {
     sinon.restore();
   });
 
-  it('cria um produto e retorna ServiceResponse corretamente', async function () {
-    const mockDBResponse = {
-      dataValues: {
+  describe('Create products POST', function () {
+    it('cria um produto e retorna ServiceResponse corretamente', async function () {
+      const mockDBResponse = {
+        dataValues: {
+          id: 6,
+          name: 'Martelo de Thor',
+          price: '30 peças de ouro',
+          userId: 1,
+        },
+      };
+
+      sinon.stub(productsModel, 'create').resolves(mockDBResponse as any);
+
+      const result = await productsService.create({
+        name: 'Martelo de Thor',
+        price: '30 peças de ouro',
+        userId: 1,
+      });
+
+      expect(result.status).to.equal('CREATED');
+      expect(result.data).to.deep.equal({
         id: 6,
         name: 'Martelo de Thor',
         price: '30 peças de ouro',
         userId: 1,
-      },
-    };
-
-    sinon.stub(productsModel, 'create').resolves(mockDBResponse as any);
-
-    const result = await productsService.create({
-      name: 'Martelo de Thor',
-      price: '30 peças de ouro',
-      userId: 1,
+      });
     });
+  })
 
-    expect(result.status).to.equal('CREATED');
-    expect(result.data).to.deep.equal({
-      id: 6,
-      name: 'Martelo de Thor',
-      price: '30 peças de ouro',
-      userId: 1,
+  describe('Get all products GET', function () {
+    it('Retorna todos os produtos com status SUCCESSFUL', async function () {
+      const mockProducts = [
+        {
+          get: () => ({ id: 1, name: 'Excalibur', price: '10 peças de ouro', userId: 1 })
+        },
+        {
+          get: () => ({ id: 2, name: 'Espada Justiceira', price: '20 peças de ouro', userId: 1 })
+        },
+      ];
+
+      sinon.stub(productsModel, 'findAll').resolves(mockProducts as any);
+
+      const result = await productsService.getAll();
+
+      expect(result.status).to.equal('SUCCESSFUL');
+      expect(result.data).to.deep.equal([
+        { id: 1, name: 'Excalibur', price: '10 peças de ouro', userId: 1 },
+        { id: 2, name: 'Espada Justiceira', price: '20 peças de ouro', userId: 1 },
+      ]);
     });
   });
 });
