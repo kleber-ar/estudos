1b5cc42ea0b75a70c8af5a2876ba5dc0b63cc697
kleber-ar <kleber-ar@hotmail.com>
2025-11-14 19:03:54 -0300

    add unit test getAll users service,controller

--- trybe/back-end/3-periodo/7-seção_typeScript/sd-034-project-trybesmith/tests/unit/controllers/users.controller.test.ts
+++ trybe/back-end/3-periodo/7-seção_typeScript/sd-034-project-trybesmith/tests/unit/controllers/users.controller.test.ts
@@ -1,18 +1,37 @@
-import chai, { expect } from 'chai';
+import { expect } from 'chai';
 import sinon from 'sinon';
-import sinonChai from 'sinon-chai';
-//import { Request, Response } from 'express';
+import usersController from '../../../src/controllers/users.controller';
+import usersService from '../../../src/services/users.service';
 
-chai.use(sinonChai);
+describe('Controller - Users', function () {
+  afterEach(() => sinon.restore());
 
-describe('UsersController', function () {
-  const req = {} as Request;
-  const res = {} as Response;
+  it('retorna 200 e lista de usuários', async function () {
+    const req = {} as any;
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    } as any;
 
-  beforeEach(function () {
-    res.status = sinon.stub().returns(res);
-    res.json = sinon.stub().returns(res);
-    sinon.restore();
+    const mockResponse = {
+      status: 'SUCCESSFUL',
+      data: [
+        { username: 'Hagar', productIds: [1, 2] },
+        { username: 'Eddie', productIds: [3, 4] },
+      ],
+    };
+
+    sinon.stub(usersService, 'getAll').resolves({
+      status: 'SUCCESSFUL',
+      data: [
+        { username: 'Hagar', productIds: [1, 2] },
+        { username: 'Eddie', productIds: [3, 4] },
+      ],
+    });
+
+    await usersController.getAll(req, res);
+
+    expect(res.status.calledWith(200)).to.be.true;
+    expect(res.json.calledWith(mockResponse.data)).to.be.true;
   });
-
 });
--- trybe/back-end/3-periodo/7-seção_typeScript/sd-034-project-trybesmith/tests/unit/services/users.service.test.ts
+++ trybe/back-end/3-periodo/7-seção_typeScript/sd-034-project-trybesmith/tests/unit/services/users.service.test.ts
@@ -1,7 +1,39 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
+import usersService from '../../../src/services/users.service';
+import UserModel from '../../../src/database/models/user.model';
+import ProductModel from '../../../src/database/models/product.model';
 
-describe('UsersService', function () {
-  beforeEach(function () { sinon.restore(); });
+describe('Service - Users', function () {
+  afterEach(() => sinon.restore());
 
+  it('retorna usuários com seus productIds', async function () {
+    const mockUsers = [
+      { get: () => ({ id: 1, username: 'Hagar' }) },
+      { get: () => ({ id: 2, username: 'Eddie' }) },
+    ];
+
+    const mockProductsUser1 = [
+      { get: () => ({ id: 1 }) },
+      { get: () => ({ id: 2 }) },
+    ];
+
+    const mockProductsUser2 = [
+      { get: () => ({ id: 3 }) },
+      { get: () => ({ id: 4 }) },
+    ];
+
+    sinon.stub(UserModel, 'findAll').resolves(mockUsers as any);
+    sinon.stub(ProductModel, 'findAll')
+      .onFirstCall().resolves(mockProductsUser1 as any)
+      .onSecondCall().resolves(mockProductsUser2 as any);
+
+    const result = await usersService.getAll();
+
+    expect(result.status).to.equal('SUCCESSFUL');
+    expect(result.data).to.deep.equal([
+      { username: 'Hagar', productIds: [1, 2] },
+      { username: 'Eddie', productIds: [3, 4] },
+    ]);
+  });
 });
