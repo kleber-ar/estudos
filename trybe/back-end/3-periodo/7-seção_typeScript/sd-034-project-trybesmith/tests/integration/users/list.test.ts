17324df8cd8279eb50a278cf974761883576723f
kleber-ar <kleber-ar@hotmail.com>
2025-11-14 19:05:51 -0300

    add int test getAll users

--- trybe/back-end/3-periodo/7-seção_typeScript/sd-034-project-trybesmith/tests/integration/users/list.test.ts
+++ trybe/back-end/3-periodo/7-seção_typeScript/sd-034-project-trybesmith/tests/integration/users/list.test.ts
@@ -1,10 +1,22 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import app from '../../../src/app';
 
 chai.use(chaiHttp);
 
-describe('GET /users', function () { 
+describe('GET /users', function () {
   beforeEach(function () { sinon.restore(); });
+  it('Retorna 200 e lista todas as pessoas usuárias', async function () {
+    const res = await chai.request(app).get('/users');
 
+    expect(res.status).to.equal(200);
+    expect(res.body).to.be.an('array');
+
+    res.body.forEach((u: any) => {
+      expect(u).to.have.property('username');
+      expect(u).to.have.property('productIds');
+      expect(u.productIds).to.be.an('array');
+    });
+  });
 });
