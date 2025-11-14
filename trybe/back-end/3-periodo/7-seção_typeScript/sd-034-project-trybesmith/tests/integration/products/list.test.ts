adbf57ba216d07e2e1a16c30c1219ff1e963fa35
kleber-ar <kleber-ar@hotmail.com>
2025-11-14 18:22:08 -0300

    add test int getAll products

--- trybe/back-end/3-periodo/7-seção_typeScript/sd-034-project-trybesmith/tests/integration/products/list.test.ts
+++ trybe/back-end/3-periodo/7-seção_typeScript/sd-034-project-trybesmith/tests/integration/products/list.test.ts
@@ -1,10 +1,17 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import app from '../../../src/app';
 
 chai.use(chaiHttp);
 
-describe('GET /products', function () { 
+describe('GET /products', function () {
   beforeEach(function () { sinon.restore(); });
+  it('Retorna 200 e a lista de produtos', async function () {
+    const response = await chai.request(app).get('/products');
 
+    expect(response.status).to.equal(200);
+    expect(response.body).to.be.an('array');
+    expect(response.body[0]).to.have.keys(['id', 'name', 'price', 'userId']);
+  });
 });
