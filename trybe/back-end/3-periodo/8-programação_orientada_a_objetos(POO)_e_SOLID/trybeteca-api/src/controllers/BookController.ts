a76bd450255f1d62cdf68c7d5e6683765a7e3e91
kleber-ar <kleber-ar@hotmail.com>
2025-11-20 15:25:57 -0300

    add controller createBook

---
+++ trybe/back-end/3-periodo/8-programação_orientada_a_objetos(POO)_e_SOLID/trybeteca-api/src/controllers/BookController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import BookService from '../services/BookService';
+
+export default class BookController {
+  constructor(
+    private bookService = new BookService(),
+  ) { }
+
+  public async createBook(req: Request, res: Response) {
+    const serviceResponse = await this.bookService.createBook(req.body);
+    res.status(201).json(serviceResponse.data);
+  }
+}
