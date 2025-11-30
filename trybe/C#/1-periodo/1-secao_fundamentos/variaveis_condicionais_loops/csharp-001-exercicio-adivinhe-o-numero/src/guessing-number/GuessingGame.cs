2bebfa75eb6436c1d15e7d7452ce8d98325ed323
kleber-ar <kleber-ar@hotmail.com>
2025-11-30 18:52:19 -0300

    add greeting message

---
+++ trybe/C#/1-periodo/1-secao_fundamentos/variaveis_condicionais_loops/csharp-001-exercicio-adivinhe-o-numero/src/guessing-number/GuessingGame.cs
@@ -0,0 +1,68 @@
+using System;
+
+namespace guessing_number;
+
+public class GuessNumber
+{
+    //In this way we are passing the random number generator by dependency injection
+    private IRandomGenerator random;
+    public GuessNumber() : this(new DefaultRandom()){}
+    public GuessNumber(IRandomGenerator obj)
+    {
+        this.random = obj;
+
+        userValue = 0;
+        randomValue = 0;
+    }
+
+    //user variables
+    public int userValue;
+    public int randomValue;
+
+    public int maxAttempts;
+    public int currentAttempts;
+
+    public int difficultyLevel;
+
+    public bool gameOver;
+
+    //1 - Imprima uma mensagem de saudação
+    public string Greet()
+    {
+        string  GreetMsg = "";
+        GreetMsg ++ "---Bem-vindo ao Guessing Game--- /n";
+        GreetMsg ++ "Para começar, tente adivinhar o número que eu pensei, entre -100 e 100!";
+        return GreetMsg;
+    }
+
+    //2 - Receba a entrada da pessoa usuária e converta para Int
+    //5 - Adicione um limite de tentativas
+    public string ChooseNumber(string userEntry)
+    {
+        throw new NotImplementedException();
+    }
+
+    //3 - Gere um número aleatório
+    public string RandomNumber()
+    {
+        throw new NotImplementedException();
+    }
+
+    //6 - Adicione níveis de dificuldade
+    public string RandomNumberWithDifficult()
+    {
+        throw new NotImplementedException();
+    }
+
+    //4 - Verifique a resposta da jogada
+    public string AnalyzePlay()
+    {
+        throw new NotImplementedException();
+    }
+
+    //7 - Adicione uma opção para reiniciar o jogo
+    public void RestartGame()
+    {
+        throw new NotImplementedException();
+    }
+}
