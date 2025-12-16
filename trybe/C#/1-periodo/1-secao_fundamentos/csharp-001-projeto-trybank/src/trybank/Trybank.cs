9056ec1747251d95d5aea22a857fcd4f4ce3328d
kleber-ar <kleber-ar@hotmail.com>
2025-12-16 17:41:36 -0300

    C# logout logic

--- trybe/C#/1-periodo/1-secao_fundamentos/csharp-001-projeto-trybank/src/trybank/Trybank.cs
+++ trybe/C#/1-periodo/1-secao_fundamentos/csharp-001-projeto-trybank/src/trybank/Trybank.cs
@@ -77,9 +77,15 @@ public class TrybankLib
     // 3. Construa a funcionalidade de fazer Logout
     public void Logout()
     {
-        throw new NotImplementedException();
-    }
+      if (!Logged)
+      {
+        throw new AccessViolationException("Usuário não está logado");
+      }
 
+      Logged = false;
+      loggedUser = -99;
+    }
+    
     // 4. Construa a funcionalidade de checar o saldo
     public int CheckBalance()
     {
