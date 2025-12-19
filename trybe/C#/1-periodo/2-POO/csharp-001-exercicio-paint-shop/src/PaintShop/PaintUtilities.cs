f4ec7b8fcda9b2c9fcb47c7e860735a2b6bc4378
kleber-ar <kleber-ar@hotmail.com>
2025-12-19 05:20:41 -0300

    C# paint_Shopping paintUtilities

--- trybe/C#/1-periodo/2-POO/csharp-001-exercicio-paint-shop/src/PaintShop/PaintUtilities.cs
+++ trybe/C#/1-periodo/2-POO/csharp-001-exercicio-paint-shop/src/PaintShop/PaintUtilities.cs
@@ -3,29 +3,39 @@ namespace PaintShop;
 // 3 - Crie uma classe estática PaintUtilities
 public static class PaintUtilities
 {
-    public static int SquareMetersPerBucket;
+    public static int BucketSizeLiters { get; set; } = 20;
+    public static int SquareMetersPerLiter { get; set; } = 10;
+
+    public static int SquareMetersPerBucket { get {
+        return SquareMetersPerLiter * BucketSizeLiters;
+    }}
 
     public static int GetNeededPaintBuckets(double area) {
-         throw new NotImplementedException();
+        return Convert.ToInt32(Math.Ceiling(area / SquareMetersPerBucket));
     }
 
     public static int GetNeededPaintBuckets(Wall wall) {
-        throw new NotImplementedException();
+        return Convert.ToInt32(Math.Ceiling(wall.PaintableArea / SquareMetersPerBucket));
     }
 
     public static int GetNeededPaintBuckets(Room room) {
-        throw new NotImplementedException();
+        double totalArea = 0;
+        foreach(var wall in room.walls)
+        {
+            totalArea += wall.PaintableArea;
+        }
+        return Convert.ToInt32(Math.Ceiling(totalArea / SquareMetersPerBucket));
     }
 
     public static decimal CalculateCost(decimal price, double area) {
-        throw new NotImplementedException();
+        return price * GetNeededPaintBuckets(area);
     }
 
     public static decimal CalculateCost(decimal price, Wall wall) {
-        throw new NotImplementedException();
+        return price * GetNeededPaintBuckets(wall);
     }
 
     public static decimal CalculateCost(decimal price, Room room) {
-        throw new NotImplementedException();
+        return price * GetNeededPaintBuckets(room);
     }
 }
