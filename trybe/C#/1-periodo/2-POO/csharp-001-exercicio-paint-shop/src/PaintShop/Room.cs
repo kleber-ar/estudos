f16f94f5f8ad7e961d554eedbc559656cb589a70
kleber-ar <kleber-ar@hotmail.com>
2025-12-19 05:19:37 -0300

    C# paint_Shopping room

--- trybe/C#/1-periodo/2-POO/csharp-001-exercicio-paint-shop/src/PaintShop/Room.cs
+++ trybe/C#/1-periodo/2-POO/csharp-001-exercicio-paint-shop/src/PaintShop/Room.cs
@@ -3,7 +3,20 @@ namespace PaintShop;
 // 2 - Crie uma classe Room
 public class Room
 {
-    public double TotalPaintableArea;
-    public Room(Wall[] walls)
-    {}
+   public Wall[] walls { get; }
+    public double TotalPaintableArea {
+        get { 
+            double totalArea = 0;
+            foreach(var wall in walls)
+            {
+                totalArea += wall.PaintableArea;
+            }
+            return totalArea;
+        }
+    }
+
+    public Room(params Wall[] walls)
+    {
+        this.walls = walls;
+    }
 }
