61c20a7be348bd94254bc1b829f2bce929fe75a8
kleber-ar <kleber-ar@hotmail.com>
2025-12-19 05:18:33 -0300

    C# paint_Shopping 1

--- trybe/C#/1-periodo/2-POO/csharp-001-exercicio-paint-shop/src/PaintShop/Wall.cs
+++ trybe/C#/1-periodo/2-POO/csharp-001-exercicio-paint-shop/src/PaintShop/Wall.cs
@@ -1,13 +1,57 @@
 ﻿namespace PaintShop;
 
-// 1 - Crie uma classe Wall
 public class Wall
 {
-    public double Width;
-    public double Height;
-    public double ExcludedArea;
-    public double PaintableArea;
+    // Width com get implícito e set explícito
+    public double Width
+    {
+        get;
+        set
+        {
+            if (value > 0)
+            {
+                field = value;
+            }
+            else
+            {
+                field = 1;
+            }
+        }
+    }
+
+    // Height com get implícito e set explícito
+    public double Height
+    {
+        get;
+        set
+        {
+            if (value > 0)
+            {
+                field = value;
+            }
+            else
+            {
+                field = 1;
+            }
+        }
+    }
+
+    // ExcludedArea com get e set implícitos
+    public double ExcludedArea { get; set; }
+
+    // PaintableArea sem set e com get explícito
+    public double PaintableArea
+    {
+        get
+        {
+            return (Width * Height) - ExcludedArea;
+        }
+    }
+
+    // Construtor
     public Wall(double width, double height)
     {
+        Width = width;
+        Height = height;
     }
 }
