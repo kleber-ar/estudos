fcca8f2a31c9000139d57b0f193328d60dd59ed4
kleber-ar <kleber-ar@hotmail.com>
2025-12-28 20:49:19 -0300

    C# Rent_Cars - req-10 add constructor Rent

--- trybe/C#/1-periodo/2-POO/csharp-001-projeto-aluguel-de-carros/src/RentCars/Models/Rent.cs
+++ trybe/C#/1-periodo/2-POO/csharp-001-projeto-aluguel-de-carros/src/RentCars/Models/Rent.cs
@@ -13,7 +13,22 @@ public class Rent
     //10 - Crie o construtor de `Rent` seguindo as regras de negócio
     public Rent(Vehicle vehicle, Person person, int daysRented)
     {
-        throw new NotImplementedException();
+        Vehicle = vehicle;
+        Person = person;
+        DaysRented = daysRented;
+
+        double calculatedPrice = vehicle.PricePerDay * daysRented;
+
+        if (person is LegalPerson)
+        {
+            calculatedPrice *= 0.9;
+        }
+
+        Price = calculatedPrice;
+        Status = RentStatus.Confirmed;
+
+        vehicle.IsRented = true;
+        person.Debit = Price;
     }
 
     //11 - Implemente os métodos de `cancelar` e `finalizar` um aluguel
