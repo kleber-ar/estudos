f51ca86d33aaf88119f3aa407c66876cd5fa5dfa
kleber-ar <kleber-ar@hotmail.com>
2026-03-04 07:08:07 -0300

    C# Trybe-hotel: add repository para conecção do DB

--- trybe/C#/trybe-hotel/src/TrybeHotel/Repository/TrybeHotelContext.cs
+++ trybe/C#/trybe-hotel/src/TrybeHotel/Repository/TrybeHotelContext.cs
@@ -2,8 +2,27 @@ using Microsoft.EntityFrameworkCore;
 using TrybeHotel.Models;
 
 namespace TrybeHotel.Repository;
+
 public class TrybeHotelContext : DbContext, ITrybeHotelContext
 {
-    public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options) { }
+    public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options)
+        : base(options) { }
+
     public TrybeHotelContext() { }
+
+    // DbSets obrigatórios
+    public DbSet<City> Cities { get; set; } = null!;
+    public DbSet<Hotel> Hotels { get; set; } = null!;
+    public DbSet<Room> Rooms { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var connectionString =
+                "Server=localhost;Database=TrybeHotel;User=SA;Password=TrybeHotel12!;TrustServerCertificate=True";
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
 }
