d0564202cb4fbb284386587f2d0852128aa13e0b
kleber-ar <kleber-ar@hotmail.com>
2026-03-08 21:31:19 -0300

    C# Trybe-hotel: REQ 6 add DTO,Repository,Controller e GET Room

--- trybe/C#/trybe-hotel/src/TrybeHotel/Dto/RoomDto.cs
+++ trybe/C#/trybe-hotel/src/TrybeHotel/Dto/RoomDto.cs
@@ -1,4 +1,16 @@
-namespace TrybeHotel.Dto {
-     public class RoomDto {
-     }
+using TrybeHotel.Dto;
+
+namespace TrybeHotel.Dto;
+
+public class RoomDto
+{
+    public int RoomId { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public int Capacity { get; set; }
+
+    public string Image { get; set; } = string.Empty;
+
+    public HotelDto Hotel { get; set; } = null!;
 }
