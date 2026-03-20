7de8521576f8fcc3fb7ba45c9e1ec8ddd7142415
kleber-ar <kleber-ar@hotmail.com>
2026-03-20 11:57:11 -0300

    C# Trybe-hotel: att test.test

--- trybe/C#/trybe-hotel/src/TrybeHotel.Test.Test/ContextTest.cs
+++ trybe/C#/trybe-hotel/src/TrybeHotel.Test.Test/ContextTest.cs
@@ -10,7 +10,32 @@ public class ContextTest : DbContext, ITrybeHotelContext
     public virtual DbSet<City> Cities { get; set; }
     public virtual DbSet<Hotel> Hotels { get; set; }
     public virtual DbSet<Room> Rooms { get; set; }
+    public virtual DbSet<User> Users { get; set; }
+    public virtual DbSet<Booking> Bookings { get; set; }
     public ContextTest(DbContextOptions<ContextTest> options) : base(options)
     { }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<City>()
+        .HasMany(c => c.Hotels)
+        .WithOne(h => h.City)
+        .HasForeignKey(h => h.CityId);
+
+
+        modelBuilder.Entity<Hotel>()
+        .HasMany(r => r.Rooms)
+        .WithOne(h => h.Hotel)
+        .HasForeignKey(r => r.HotelId);
+
+        modelBuilder.Entity<Room>()
+            .HasMany(b => b.Bookings)
+            .WithOne(r => r.Room)
+            .HasForeignKey(b => b.RoomId);
+
+        modelBuilder.Entity<User>()
+            .HasMany(b => b.Bookings)
+            .WithOne(u => u.User)
+            .HasForeignKey(b => b.UserId);
+    }
 }
