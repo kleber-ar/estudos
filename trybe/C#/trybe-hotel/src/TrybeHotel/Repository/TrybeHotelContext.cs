9c4e7185b74ccfc9fc901cb1cca59b2a3d1c8be6
kleber-ar <kleber-ar@hotmail.com>
2026-04-03 21:40:31 -0300

    C# Trybe-hotel-fase-c att repository context

--- trybe/C#/trybe-hotel/src/TrybeHotel/Repository/TrybeHotelContext.cs
+++ trybe/C#/trybe-hotel/src/TrybeHotel/Repository/TrybeHotelContext.cs
@@ -5,26 +5,27 @@ namespace TrybeHotel.Repository;
 
 public class TrybeHotelContext : DbContext, ITrybeHotelContext
 {
+    public DbSet<City> Cities { get; set; } = null!;
+    public DbSet<Hotel> Hotels { get; set; } = null!;
+    public DbSet<Room> Rooms { get; set; } = null!;
+    public DbSet<User> Users { get; set; } = null!;
+    public DbSet<Booking> Bookings { get; set; } = null!;
+
     public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options)
-        : base(options) { }
+        : base(options)
+    {
+        Seeder.SeedUserAdmin(this);
+    }
 
     public TrybeHotelContext() { }
 
-    // DbSets obrigatórios
-    public DbSet<City> Cities { get; set; }
-    public DbSet<Hotel> Hotels { get; set; }
-    public DbSet<Room> Rooms { get; set; }
-    public DbSet<User> Users { get; set; }
-    public DbSet<Booking> Bookings { get; set; }
-
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        if (!optionsBuilder.IsConfigured)
-        {
-            var connectionString =
-                "Server=localhost;Database=TrybeHotel;User=SA;Password=TrybeHotel12!;TrustServerCertificate=True";
+        // ❌ NÃO configurar banco aqui
+    }
 
-            optionsBuilder.UseSqlServer(connectionString);
-        }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
     }
 }
