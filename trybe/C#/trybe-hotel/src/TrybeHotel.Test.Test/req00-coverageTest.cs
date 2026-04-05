ac1f032149b251ca3b93acb43db253fe2595678a
kleber-ar <kleber-ar@hotmail.com>
2026-04-05 08:15:11 -0300

    C# Trybe-hotel-fase-c fix tests coverage

--- trybe/C#/trybe-hotel/src/TrybeHotel.Test.Test/req00-coverageTest.cs
+++ trybe/C#/trybe-hotel/src/TrybeHotel.Test.Test/req00-coverageTest.cs
@@ -8,7 +8,7 @@ using System.Xml;
 using System.IO;
 
 
-public class TestReq19
+public class TestReq00
 {
     [Fact]
     [Trait("Category", "10. Desenvolva testes que cubram no mínimo 40% de linhas")]
