f36b59c77230af74b3ed9b18ebfa5f9f0877aac9
kleber-ar <kleber-ar@hotmail.com>
2026-04-05 16:50:48 -0300

    C# Trybe-hotel-fase-d REQ1 add getStatus

---
+++ trybe/C#/trybe-hotel/src/TrybeHotel/Controllers/StatusController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace TrybeHotel.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class StatusController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetStatus()
+        {
+            return Ok(new { message = "online" });
+        }
+    
+    }
+}
