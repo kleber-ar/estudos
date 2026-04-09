72b77a1aecb98717cc285a66a4c52e3e7b4dff16
kleber-ar <kleber-ar@hotmail.com>
2026-04-09 06:35:05 -0300

    C# Trybets REQ4 add Odds

--- trybe/C#/trybets/src/TryBets.Odds/Repository/OddRepository.cs
+++ trybe/C#/trybets/src/TryBets.Odds/Repository/OddRepository.cs
@@ -15,6 +15,32 @@ public class OddRepository : IOddRepository
 
     public Match Patch(int MatchId, int TeamId, string BetValue)
     {
-        throw new NotImplementedException();
+        string betValueConverted = BetValue.Replace(',', '.');
+        
+        decimal betValueDecimal = Decimal.Parse(betValueConverted, CultureInfo.InvariantCulture);
+
+            var match = _context.Matches.FirstOrDefault(m => m.MatchId == MatchId);
+
+            if (match == null)
+            {
+                throw new Exception($"Match with id {MatchId} not found.");
+            }
+
+            if (TeamId == match.MatchTeamAId)
+            {
+                match.MatchTeamAValue += betValueDecimal;
+            }
+            else if (TeamId == match.MatchTeamBId)
+            {
+                match.MatchTeamBValue += betValueDecimal;
+            }
+            else
+            {
+                throw new Exception($"Team with id {TeamId} not found in match {MatchId}.");
+            }
+
+            _context.SaveChanges();
+
+            return match;
     }
 }
