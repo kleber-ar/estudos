63f268155cf5d8c1f48d2ffae159b5da9abed33c
kleber-ar <kleber-ar@hotmail.com>
2026-04-17 11:42:44 -0300

    Task Manager - add: test services rises error to created invalid name

--- trybe/python/1-periodo/1-Debug_com_python/task_manager/tests/test_services.py
+++ trybe/python/1-periodo/1-Debug_com_python/task_manager/tests/test_services.py
@@ -1,3 +1,4 @@
+import pytest
 from task_manager import services
 from unittest.mock import patch, Mock
 
@@ -11,6 +12,8 @@ def test_get_tasks_should_same_as_database_read():
     assert res == mock_db_read.return_value
     mock_db_read.assert_called_once()
 
+# Pode Instalar O faker para ter dados falsos
+
 
 def test_create_task_should_call_database_save():
     mock_db_save = Mock()
@@ -22,3 +25,12 @@ def test_create_task_should_call_database_save():
     mock_db_save.assert_called_once_with(
         [{"id": 1, "name": fake_name, "completed": False}]
     )
+
+
+def test_create_task_with_invalid_name_should_raise_error():
+
+    with pytest.raises(TypeError, match="Task name must be a string"):
+        services.create_task(123)
+
+    with pytest.raises(ValueError, match="Task name must have at least 3 characters"):
+        services.create_task("")
