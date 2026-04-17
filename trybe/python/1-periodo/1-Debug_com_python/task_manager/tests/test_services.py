6d239486d4c5b6e62097b4720830e85b44b01a9e
kleber-ar <kleber-ar@hotmail.com>
2026-04-17 11:10:39 -0300

    Task Manager - add: test services to create db with mock

--- trybe/python/1-periodo/1-Debug_com_python/task_manager/tests/test_services.py
+++ trybe/python/1-periodo/1-Debug_com_python/task_manager/tests/test_services.py
@@ -10,3 +10,15 @@ def test_get_tasks_should_same_as_database_read():
 
     assert res == mock_db_read.return_value
     mock_db_read.assert_called_once()
+
+
+def test_create_task_should_call_database_save():
+    mock_db_save = Mock()
+
+    fake_name = "João"
+    with patch("task_manager.database.save", mock_db_save):
+        services.create_task(fake_name)
+
+    mock_db_save.assert_called_once_with(
+        [{"id": 1, "name": fake_name, "completed": False}]
+    )
