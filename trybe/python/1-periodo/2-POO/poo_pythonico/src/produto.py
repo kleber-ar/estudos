8cd7ef3794030fd7e800a4e50b24ae38d4029449
kleber-ar <kleber-ar@hotmail.com>
2026-04-24 08:07:34 -0300

    POO Pythonico: add produto

--- trybe/python/1-periodo/2-POO/poo_pythonico/src/produto.py
+++ trybe/python/1-periodo/2-POO/poo_pythonico/src/produto.py
@@ -2,19 +2,28 @@ class Produto:
     def __init__(
         self, nome: str, codigo: str, preco: float, quantidade: int
     ) -> None:
-        pass
+        self.__nome = nome
+        self.__codigo = codigo
+        self.__preco = preco
+        self.__quantidade = quantidade
 
     def get_preco(self) -> float:
-        pass
+        return self.__preco
 
     def get_quantidade(self) -> int:
-        pass
+        return self.__quantidade
 
     def atualizar_preco_do_produto(self, novo_preco: float) -> None:
-        pass
+        if novo_preco < 0:
+            raise ValueError("O preço não pode ser negativo.")
+        self.__preco = novo_preco
 
     def adicionar_estoque_do_produto(self, quantidade: int) -> None:
-        pass
+        self.__quantidade += quantidade
 
     def remover_estoque_do_produto(self, quantidade: int) -> None:
-        pass
+        if self.__quantidade < quantidade:
+            raise ValueError(
+                "Quant. informada é maior que a quantidade disponivel em estoque."
+            )
+        self.__quantidade -= quantidade
