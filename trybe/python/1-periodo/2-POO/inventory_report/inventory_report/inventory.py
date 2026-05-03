f89e14a21e40d9496bf200d065854f69c74d4312
kleber-ar <kleber-ar@hotmail.com>
2026-05-03 20:52:29 -0300

    inventory report :REQ5 add class Inventory

--- trybe/python/1-periodo/2-POO/inventory_report/inventory_report/inventory.py
+++ trybe/python/1-periodo/2-POO/inventory_report/inventory_report/inventory.py
@@ -1,2 +1,17 @@
+from typing import List, Optional
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self, data: Optional[List[Product]] = None):
+        if data is None:
+            self._data = []
+        else:
+            self._data = data
+
+    @property
+    def data(self) -> List[Product]:
+        return self._data.copy()
+
+    def add_data(self, data: List[Product]) -> None:
+        self._data.extend(data)
