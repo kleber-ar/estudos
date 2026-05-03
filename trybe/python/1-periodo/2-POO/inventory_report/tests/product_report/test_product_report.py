da4b487b12696b5232f8e5feb95489a5bad43d24
kleber-ar <kleber-ar@hotmail.com>
2026-05-03 20:03:38 -0300

    inventory report :add test to report product

--- trybe/python/1-periodo/2-POO/inventory_report/tests/product_report/test_product_report.py
+++ trybe/python/1-periodo/2-POO/inventory_report/tests/product_report/test_product_report.py
@@ -1,2 +1,22 @@
-def test_product_report() -> None:
-    raise NotImplementedError
+from inventory_report.product import Product
+
+
+def test_product_report():
+    product = Product(
+        "1",
+        "farinha",
+        "Farinini",
+        "01-05-2021",
+        "02-06-2023",
+        "TY68 409C JJ43 ASD1 PL2F",
+        "precisa ser armazenado em local protegido da luz",
+    )
+
+    expected = (
+        "The product 1 - farinha with serial number TY68 409C JJ43 ASD1 PL2F "
+        "manufactured on 01-05-2021 by the company Farinini "
+        "valid until 02-06-2023 must be stored according to the following "
+        "instructions: precisa ser armazenado em local protegido da luz."
+    )
+
+    assert str(product) == expected
