622f151218d4db53b644b536ff9ce4d43d6faf8b
kleber-ar <kleber-ar@hotmail.com>
2026-05-03 19:52:32 -0300

    inventory report :add test to creat product

--- trybe/python/1-periodo/2-POO/inventory_report/tests/product/test_product.py
+++ trybe/python/1-periodo/2-POO/inventory_report/tests/product/test_product.py
@@ -1,2 +1,21 @@
-def test_create_product() -> None:
-    raise NotImplementedError
+from inventory_report.product import Product
+
+
+def test_create_product():
+    product = Product(
+        "1",
+        "Produto Y",
+        "Empresa X",
+        "2023-01-01",
+        "2025-01-01",
+        "ABC123",
+        "Manter em local seco",
+    )
+
+    assert product.id == "1"
+    assert product.company_name == "Empresa X"
+    assert product.product_name == "Produto Y"
+    assert product.manufacturing_date == "2023-01-01"
+    assert product.expiration_date == "2025-01-01"
+    assert product.serial_number == "ABC123"
+    assert product.storage_instructions == "Manter em local seco"
