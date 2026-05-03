73ea0b3c95b5c1845671e7a702091c32e3dc96b2
kleber-ar <kleber-ar@hotmail.com>
2026-05-03 20:24:28 -0300

    inventory report :REQ3 add class importer

--- trybe/python/1-periodo/2-POO/inventory_report/inventory_report/importers.py
+++ trybe/python/1-periodo/2-POO/inventory_report/inventory_report/importers.py
@@ -1,8 +1,16 @@
-from typing import Dict, Type
+from abc import ABC, abstractmethod
+from typing import Dict, List, Type
+
+from inventory_report.product import Product
 
 
-class Importer:
-    pass
+class Importer(ABC):
+    def __init__(self, path: str):
+        self.path = path
+
+    @abstractmethod
+    def import_data(self) -> List[Product]:
+        pass
 
 
 class JsonImporter:
