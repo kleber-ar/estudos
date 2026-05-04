4e598fd794dbe8d5168bfd415f03ea6bc27e5f66
kleber-ar <kleber-ar@hotmail.com>
2026-05-03 21:52:49 -0300

    inventory report :REQ10 add cli

--- trybe/python/1-periodo/2-POO/inventory_report/inventory_report/cli/input_handler.py
+++ trybe/python/1-periodo/2-POO/inventory_report/inventory_report/cli/input_handler.py
@@ -1,9 +1,32 @@
 from typing import List
+from inventory_report.inventory import Inventory
+from inventory_report.importers import IMPORTERS
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
 
 
 def process_report_request(file_paths: List[str], report_type: str) -> str:
-    """
-    Process the report given a list of file paths and a report type,
-    and returns the result.
-    """
-    raise NotImplementedError
+    # escolhe o tipo de relatório
+    if report_type == "simple":
+        report = SimpleReport()
+    elif report_type == "complete":
+        report = CompleteReport()
+    else:
+        raise ValueError("Report type is invalid.")
+
+    # processa cada arquivo
+    for path in file_paths:
+        extension = path.split(".")[-1]
+
+        if extension not in IMPORTERS:
+            continue  # ignora arquivos inválidos
+
+        importer_class = IMPORTERS[extension]
+        importer = importer_class(path)
+
+        products = importer.import_data()
+
+        inventory = Inventory(products)
+        report.add_inventory(inventory)
+
+    return report.generate()
