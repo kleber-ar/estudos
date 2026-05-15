6998222615e303733bb6bd376d6d625fc62e7b39
kleber-ar <kleber-ar@hotmail.com>
2026-05-15 17:54:43 -0300

    tech news: REQ4 add scrap_news

--- trybe/python/1-periodo/3-Raspagem/tech_news/tech_news/scraper.py
+++ trybe/python/1-periodo/3-Raspagem/tech_news/tech_news/scraper.py
@@ -53,8 +53,47 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    selector = Selector(text=html_content)
+
+    url = selector.css(
+        'link[rel="canonical"]::attr(href)'
+    ).get()
+
+    title = selector.css(
+        "h1.entry-title::text"
+    ).get().strip()
+
+    timestamp = selector.css(
+        "li.meta-date::text"
+    ).get()
+
+    writer = selector.css(
+        "span.author a::text"
+    ).get()
+
+    reading_time = selector.css(
+        "li.meta-reading-time::text"
+    ).re_first(r"(\d+)")
+
+    summary = "".join(
+        selector.css(
+            "div.entry-content > p:first-of-type *::text"
+        ).getall()
+    ).strip()
+
+    category = selector.css(
+        "span.label::text"
+    ).get()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "reading_time": int(reading_time),
+        "summary": summary,
+        "category": category,
+    }
 
 
 # Requisito 5
