d28153f8bfbcfd3a0ecb366ee6fb2e8252dd68b1
kleber-ar <kleber-ar@hotmail.com>
2026-05-15 16:27:13 -0300

    tech news: REQ3 add next_page

--- trybe/python/1-periodo/3-Raspagem/tech_news/tech_news/scraper.py
+++ trybe/python/1-periodo/3-Raspagem/tech_news/tech_news/scraper.py
@@ -42,7 +42,13 @@ def scrape_updates(html_content):
 # Requisito 3
 def scrape_next_page_link(html_content):
     """Seu código deve vir aqui"""
-    raise NotImplementedError
+    selector = Selector(html_content)
+
+    next_page = selector.css(
+        "a.next.page-numbers::attr(href)"
+    ).get()
+
+    return next_page
 
 
 # Requisito 4
