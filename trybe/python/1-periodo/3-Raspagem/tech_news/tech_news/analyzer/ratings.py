804f8de4d5f9fd190ac568c478faccb114fd7865
kleber-ar <kleber-ar@hotmail.com>
2026-05-16 20:52:44 -0300

    tech news: REQ10 add top_5_category

--- trybe/python/1-periodo/3-Raspagem/tech_news/tech_news/analyzer/ratings.py
+++ trybe/python/1-periodo/3-Raspagem/tech_news/tech_news/analyzer/ratings.py
@@ -1,4 +1,29 @@
+from tech_news.database import db
+
 # Requisito 10
+
+
 def top_5_categories():
     """Seu código deve vir aqui"""
-    raise NotImplementedError
+    categories = db.news.aggregate([
+        {
+            "$group": {
+                "_id": "$category",
+                "count": {"$sum": 1},
+            }
+        },
+        {
+            "$sort": {
+                "count": -1,
+                "_id": 1,
+            }
+        },
+        {
+            "$limit": 5
+        }
+    ])
+
+    return [
+        category["_id"]
+        for category in categories
+    ]
