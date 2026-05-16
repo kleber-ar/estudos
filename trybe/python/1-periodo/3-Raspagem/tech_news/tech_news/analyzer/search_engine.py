dfa8bff6d2dc1b5f82416e10a736a400448e6dad
kleber-ar <kleber-ar@hotmail.com>
2026-05-16 20:39:32 -0300

    tech news: REQ7 add search_by_title

--- trybe/python/1-periodo/3-Raspagem/tech_news/tech_news/analyzer/search_engine.py
+++ trybe/python/1-periodo/3-Raspagem/tech_news/tech_news/analyzer/search_engine.py
@@ -1,7 +1,18 @@
+from tech_news.database import db
+
 # Requisito 7
+
+
 def search_by_title(title):
     """Seu código deve vir aqui"""
-    raise NotImplementedError
+    news = db.news.find(
+        {"title": {"$regex": title, "$options": "i"}}
+    )
+
+    return [
+        (new["title"], new["url"])
+        for new in news
+    ]
 
 
 # Requisito 8
