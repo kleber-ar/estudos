1c7077e8118256293245e818267adfc506b70fb4
kleber-ar <kleber-ar@hotmail.com>
2026-05-26 09:25:31 -0300

    project-manager taskForm taskController testForm

--- trybe/python/2-periodo/1-flask/python-conteudo-flask-project-manager/src/controllers/project_controller.py
+++ trybe/python/2-periodo/1-flask/python-conteudo-flask-project-manager/src/controllers/project_controller.py
@@ -1,9 +1,13 @@
 from flask import Blueprint, render_template, request, redirect
-# ...
+from models.projectModel import ProjectModel
+from models.querys import _project_id, _task_id
 
-# def _get_project_or_task(id):
-#     project = ProjectModel.find(id)
-#     return [task.to_dict() for task in project]
+project_controller = Blueprint('project', __name__)
+
+
+def _get_project_or_task(id):
+    project = ProjectModel.find(id)
+    return [task.to_dict() for task in project]
 
 
 def _format_date(date):
@@ -26,7 +30,24 @@ def _save_task(req, id_projeto, nome_projeto):
     task = ProjectModel(deadline)
     task.save()
 
-# ...
+
+@project_controller.route("/")
+@project_controller.route("/projects")
+def home():
+    projects = ProjectModel.separate_projects()
+    return render_template("home.html", projects=projects)
+
+
+@project_controller.route("/projects/<id>")
+def project(id):
+    project = _get_project_or_task(_project_id(id))
+    return render_template("project.html", project=project)
+
+
+@project_controller.route("/task/<id>")
+def task(id):
+    task = _get_project_or_task(_task_id(id))
+    return render_template("task.html", task=task[0])
 
 
 @project_controller.route(
