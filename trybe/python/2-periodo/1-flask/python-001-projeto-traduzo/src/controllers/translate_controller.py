7b7cb7a1167b89c2d10937dd205e03a35c0e8e3d
kleber-ar <kleber-ar@hotmail.com>
2026-05-31 13:04:07 -0300

    traduzo - REQ4/5 translate controller get e post

--- trybe/python/2-periodo/1-flask/python-001-projeto-traduzo/src/controllers/translate_controller.py
+++ trybe/python/2-periodo/1-flask/python-001-projeto-traduzo/src/controllers/translate_controller.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template
-# from deep_translator import GoogleTranslator
-# from models.language_model import LanguageModel
+from flask import Blueprint, render_template, request
+from deep_translator import GoogleTranslator
+from models.language_model import LanguageModel
 # from models.history_model import HistoryModel
 
 
@@ -10,10 +10,37 @@ translate_controller = Blueprint("translate_controller", __name__)
 # Reqs. 4 e 5
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        text_to_translate = request.form["text-to-translate"]
+        translate_from = request.form["translate-from"]
+        translate_to = request.form["translate-to"]
 
+        translated = GoogleTranslator(
+            source=translate_from,
+            target=translate_to,
+        ).translate(text_to_translate)
+
+        return render_template(
+            "index.html",
+            languages=LanguageModel.find(),
+            text_to_translate=text_to_translate,
+            translate_from=translate_from,
+            translate_to=translate_to,
+            translated=translated,
+        )
+
+    return render_template(
+        "index.html",
+        languages=LanguageModel.find(),
+        text_to_translate="O que deseja traduzir?",
+        translate_from="pt",
+        translate_to="en",
+        translated="What do you want to translate?",
+    )
 
 # Req. 6
+
+
 @translate_controller.route("/reverse", methods=["POST"])
 def reverse():
     raise NotImplementedError
