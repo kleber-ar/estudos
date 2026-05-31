5dab902767578f020cc0bc9b16211e57561767a3
kleber-ar <kleber-ar@hotmail.com>
2026-05-31 13:50:34 -0300

    traduzo - REQ7 test resquestHistory

--- trybe/python/2-periodo/1-flask/python-001-projeto-traduzo/tests/models/history/test_history_model.py
+++ trybe/python/2-periodo/1-flask/python-001-projeto-traduzo/tests/models/history/test_history_model.py
@@ -1,7 +1,19 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
 def test_request_history():
-    raise NotImplementedError
+    histories_json = HistoryModel.list_as_json()
+
+    histories = json.loads(histories_json)
+
+    assert len(histories) == 2
+
+    assert histories[0]["text_to_translate"] == "Hello, I like videogame"
+    assert histories[0]["translate_from"] == "en"
+    assert histories[0]["translate_to"] == "pt"
+
+    assert histories[1]["text_to_translate"] == "Do you love music?"
+    assert histories[1]["translate_from"] == "en"
+    assert histories[1]["translate_to"] == "pt"
