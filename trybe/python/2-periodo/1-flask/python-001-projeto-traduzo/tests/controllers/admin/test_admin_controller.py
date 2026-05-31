f0a0648efd89f895f91f19415a374737d20fbe35
kleber-ar <kleber-ar@hotmail.com>
2026-05-31 14:40:53 -0300

    traduzo - REQ9 test to delete History

--- trybe/python/2-periodo/1-flask/python-001-projeto-traduzo/tests/controllers/admin/test_admin_controller.py
+++ trybe/python/2-periodo/1-flask/python-001-projeto-traduzo/tests/controllers/admin/test_admin_controller.py
@@ -1,6 +1,45 @@
-# from src.models.history_model import HistoryModel
-# from src.models.user_model import UserModel
+from src.models.history_model import HistoryModel
+from src.models.user_model import UserModel
 
 
 def test_history_delete(app_test):
-    raise NotImplementedError
+    user = UserModel(
+        {
+            "name": "admin",
+            "token": "token-teste",
+        }
+    )
+    user.save()
+
+    history1 = HistoryModel(
+        {
+            "text_to_translate": "Hello",
+            "translate_from": "en",
+            "translate_to": "pt",
+        }
+    )
+    history1.save()
+
+    history2 = HistoryModel(
+        {
+            "text_to_translate": "World",
+            "translate_from": "en",
+            "translate_to": "pt",
+        }
+    )
+    history2.save()
+
+    before = len(HistoryModel.find())
+
+    response = app_test.delete(
+        f"/admin/history/{history1.data['_id']}",
+        headers={
+            "Authorization": "token-teste",
+            "User": "admin",
+        },
+    )
+
+    after = len(HistoryModel.find())
+
+    assert response.status_code == 204
+    assert after == before - 1
