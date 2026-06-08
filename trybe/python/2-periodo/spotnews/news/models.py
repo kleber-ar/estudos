bc04d27bb9b805d7dd206189d3e2a3de8de0b038
kleber-ar <kleber-ar@hotmail.com>
2026-06-08 05:18:01 -0300

    PY_Django-spotnews: REQ3 add model News with validade

--- trybe/python/2-periodo/spotnews/news/models.py
+++ trybe/python/2-periodo/spotnews/news/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
+
+def validate_title(value):
+    if len(value.split()) < 2:
+        raise ValidationError(
+            "O título deve conter pelo menos 2 palavras."
+        )
 
 
 class Category(models.Model):
@@ -16,3 +24,24 @@ class User(models.Model):
 
     def __str__(self) -> str:
         return str(self.name)
+
+
+class News(models.Model):
+    title = models.CharField(
+        max_length=200,
+        validators=[validate_title],
+    )
+    content = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateField()
+    image = models.ImageField(
+        upload_to="img/",
+        blank=True,
+    )
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self) -> str:
+        return str(self.title)
