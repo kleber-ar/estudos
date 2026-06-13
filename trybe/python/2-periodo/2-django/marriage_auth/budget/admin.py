193c43bc70bc3f4ab52aa4c60dc6779d38faf81a
kleber-ar <kleber-ar@hotmail.com>
2026-06-13 18:05:54 -0300

    marryme_auth add: admin register

--- trybe/python/2-periodo/2-django/marriage_auth/budget/admin.py
+++ trybe/python/2-periodo/2-django/marriage_auth/budget/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Vendor, Marriage, Budget
 
-# Register your models here.
+admin.site.register(Vendor)
+admin.site.register(Marriage)
+admin.site.register(Budget)
