e3b5b5dcac2547fd57e636c3dbf8cbf446927a34
kleber-ar <kleber-ar@hotmail.com>
2026-06-13 18:53:41 -0300

    marryme_auth add: config relação de 1:1

--- trybe/python/2-periodo/2-django/marriage_auth/budget/serializers.py
+++ trybe/python/2-periodo/2-django/marriage_auth/budget/serializers.py
@@ -1,9 +1,8 @@
-
 from rest_framework import serializers
 from .models import Vendor, Marriage, Budget
 
 
-class VendorSerializer(serializers.ModelSerializer):
+class VendorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Vendor
         fields = ["id", "name", "price"]
@@ -15,9 +14,21 @@ class BudgetSerializer(serializers.ModelSerializer):
         fields = ["id", "vendors", "marriage"]
 
 
+class NestedBudgetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Budget
+        fields = ["id", "vendors"]
+
+
 class MarriageSerializer(serializers.ModelSerializer):
-    budget = BudgetSerializer()
+    budget = NestedBudgetSerializer()
 
     class Meta:
         model = Marriage
         fields = ["id", "codename", "date", "budget"]
+
+    def create(self, validated_data):
+        budget_data = validated_data.pop('budget')
+        budget_data['marriage'] = Marriage.objects.create(**validated_data)
+        BudgetSerializer().create(validated_data=budget_data)
+        return budget_data['marriage']
