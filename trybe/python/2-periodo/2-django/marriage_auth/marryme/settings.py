a06b8d388930412e8eed6414bd42c6d305bf91fa
kleber-ar <kleber-ar@hotmail.com>
2026-06-13 17:49:59 -0300

    marryme_auth fix: HOST to local

--- trybe/python/2-periodo/2-django/marriage_auth/marryme/settings.py
+++ trybe/python/2-periodo/2-django/marriage_auth/marryme/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "NAME": "marryme",
         "USER": "root",
         "PASSWORD": "root",
-        "HOST": "db",
+        "HOST": "127.0.0.1",
         "PORT": "3306",
     }
 }
