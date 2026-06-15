fc191de27784259f076ee584b4cfab56520ed11b
kleber-ar <kleber-ar@hotmail.com>
2026-06-15 04:43:50 -0300

    java fundamentos init

---
+++ trybe/java/1-periodo/fundamentos/smartHouse.java
@@ -0,0 +1,34 @@
+package com.trybe.java;
+
+/*Classe*/
+public class SmartHouse {
+
+  /*Atributos */
+
+  public boolean lampadaLigada;
+
+  /*Construtor da classe SmartHouse.*/
+  public SmartHouse() {
+    this.lampadaLigada = false;
+  }
+
+  /*Método para ligar uma lâmpada.*/
+  public void ligarLampada() {
+    this.lampadaLigada = true;
+  }
+
+  /*Método para desligar uma lâmpada.*/
+  public void desligarLampada() {
+    this.lampadaLigada = false;
+  }
+
+  /*Método para verificar se uma lâmpada está ligada ou não.*/
+  public boolean isLampadaLigada() {
+    return lampadaLigada;
+  }
+
+  /*Método que simula conexão com a Internet.*/
+  public boolean conectarInternet(double rate) {
+    return rate > 0.5;
+  }
+}
