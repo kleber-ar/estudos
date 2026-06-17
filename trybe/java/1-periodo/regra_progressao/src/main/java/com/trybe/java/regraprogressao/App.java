a4e998458f864409358ec9301d0c6d7a0522ff90
kleber-ar <kleber-ar@hotmail.com>
2026-06-17 06:42:13 -0300

    java regra_progressao: REQ1 cadastro de atividades

--- trybe/java/1-periodo/regra_progressao/src/main/java/com/trybe/java/regraprogressao/App.java
+++ trybe/java/1-periodo/regra_progressao/src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,13 +1,34 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
- * App.
+ * Classe principal da aplicação.
  */
 public class App {
 
   /**
-   * Metodo main.
+   * Método principal responsável por executar o programa.
+   *
+   * @param args argumentos recebidos na execução
    */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    int quantidade = Integer.parseInt(scanner.nextLine());
+
+    String[] atividades = new String[quantidade];
+    int[] pesos = new int[quantidade];
+
+    for (int i = 0; i < quantidade; i++) {
+      System.out.println("Digite o nome da atividade " + (i + 1) + ":");
+      atividades[i] = scanner.nextLine();
+
+      System.out.println("Digite o peso da atividade " + (i + 1) + ":");
+      pesos[i] = Integer.parseInt(scanner.nextLine());
+    }
+
+    scanner.close();
   }
 }
