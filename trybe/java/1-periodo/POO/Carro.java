a20c12953bb68789b1e2cd2e83b88253b6d99bd2
kleber-ar <kleber-ar@hotmail.com>
2026-06-18 13:47:20 -0300

    java POO: add classe carro

---
+++ trybe/java/1-periodo/POO/Carro.java
@@ -0,0 +1,16 @@
+
+public class Carro {
+  String marca;
+  String modelo;
+  int ano;
+
+  public Carro(String marca, String modelo, int ano) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.ano = ano;
+  }
+
+  public void exibirInformacoes() {
+    System.out.println("Marca: " + marca + ", Modelo: " + modelo + ", Ano: " + ano);
+  }
+}
