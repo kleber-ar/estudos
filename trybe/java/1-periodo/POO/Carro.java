5d445d01921c7d67b45f1b9d6fb0adad94347d33
kleber-ar <kleber-ar@hotmail.com>
2026-06-18 14:24:39 -0300

    java POO: atributos privados e add get e set

--- trybe/java/1-periodo/POO/Carro.java
+++ trybe/java/1-periodo/POO/Carro.java
@@ -1,8 +1,8 @@
 
 public class Carro {
-  String marca;
-  String modelo;
-  int ano;
+  private String marca;
+  private String modelo;
+  private int ano;
 
   public Carro(String marca, String modelo, int ano) {
     this.marca = marca;
@@ -12,5 +12,29 @@ public class Carro {
 
   public void exibirInformacoes() {
     System.out.println("Marca: " + marca + ", Modelo: " + modelo + ", Ano: " + ano);
+  }
+
+  public String getMarca() {
+    return marca;
+  }
+  
+  public void setMarca(String marca) {
+    this.marca = marca;
+  }
+  
+  public String getModelo() {
+    return modelo;
+  }
+  
+  public void setModelo(String modelo) {
+    this.modelo = modelo;
+  }
+  
+  public int getAno() {
+    return ano;
+  }
+  
+  public int setAno(String ano) {
+    this.ano = ano;
   }
 }
