233ffe1446c2ebe90a4b57e4e83cb53b041975c1
kleber-ar <kleber-ar@hotmail.com>
2026-06-19 18:24:55 -0300

    java POO: RH

--- trybe/java/1-periodo/POO/exercicios_poo/src/main/java/com/trybe/acc/recursoshumanos/PessoaFuncionaria.java
+++ trybe/java/1-periodo/POO/exercicios_poo/src/main/java/com/trybe/acc/recursoshumanos/PessoaFuncionaria.java
@@ -1,8 +1,57 @@
 package com.trybe.acc.recursoshumanos;
-
 /**
- * Classe que representa uma pessoa funcionária no sistema de recursos humanos.
- */
-public class PessoaFuncionaria {
+  * Classe que representa uma pessoa funcionária no sistema de recursos humanos.
+  */
+ public class PessoaFuncionaria {
+   private String nomeCompleto;
+   private String cpf;
+   private String endereco;
+   private double salario;
 
-}
+   /**
+    * Construtor para a classe PessoaFuncionaria.
+    *
+    * @param nomeEntrada String do nome completo
+    * @param cpfEntrada String do CPF
+    * @param enderecoEntrada String do endereço completo
+    * @param salarioEntrada Double que representa o salário
+    */
+
+   public PessoaFuncionaria(String nomeEntrada, String cpfEntrada, String enderecoEntrada,
+       double salarioEntrada) {
+     nomeCompleto = nomeEntrada;
+     cpf = cpfEntrada;
+     endereco = enderecoEntrada;
+     salario = salarioEntrada;
+   }
+
+
+   // getters e setters
+   public String getNomeCompleto() {
+     return nomeCompleto;
+   }
+
+   public void setNomeCompleto(String nomeCompleto) {
+     this.nomeCompleto = nomeCompleto;
+   }
+
+   public String getEndereco() {
+     return endereco;
+   }
+
+   public void setEndereco(String endereco) {
+     this.endereco = endereco;
+   }
+
+   public double getSalario() {
+     return salario;
+   }
+
+   public void setSalario(double salario) {
+     this.salario = salario;
+   }
+
+   public String getCpf() {
+     return cpf;
+   }
+ }
