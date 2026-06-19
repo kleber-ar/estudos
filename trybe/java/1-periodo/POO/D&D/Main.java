4dd60e171ceb0d69a85405d66bb13b10e8b4de14
kleber-ar <kleber-ar@hotmail.com>
2026-06-19 15:37:50 -0300

    java POO: D&D classe e herança

---
+++ trybe/java/1-periodo/POO/D&D/Main.java
@@ -0,0 +1,13 @@
+package com.betrybe.rpg;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Warrior warrior = new Warrior();
+    warrior.setName("Aragorn");
+    warrior.setRace("Humano"); 
+    warrior.setWeapon("Espada"); 
+
+    System.out.println(warrior.getName() + " é da raça " + warrior.getRace() + " e utiliza " + warrior.getWeapon());
+  }
+}
