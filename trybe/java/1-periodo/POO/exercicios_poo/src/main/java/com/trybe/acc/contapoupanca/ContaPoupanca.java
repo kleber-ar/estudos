145c31fd53d90255516f4ebe68dba4e3c2947bb3
kleber-ar <kleber-ar@hotmail.com>
2026-06-19 18:27:45 -0300

    java POO: CP

--- trybe/java/1-periodo/POO/exercicios_poo/src/main/java/com/trybe/acc/contapoupanca/ContaPoupanca.java
+++ trybe/java/1-periodo/POO/exercicios_poo/src/main/java/com/trybe/acc/contapoupanca/ContaPoupanca.java
@@ -1,5 +1,30 @@
 package com.trybe.acc.contapoupanca;
 
-public class ContaPoupanca {
+ public class ContaPoupanca {
+   // Atributos da classe ContaPoupanca
+   String titularConta;
+   double saldo;
 
-}
+   // Construtor da classe ContaPoupanca
+   public ContaPoupanca(String nome, double valor) {
+     titularConta = nome;
+     saldo = valor;
+   }
+
+   // Métodos da classe ContaPoupanca
+   public void depositar(double valor) {
+     saldo = saldo + valor;
+   }
+
+   public void sacar(double valor) {
+     saldo = saldo - valor;
+   }
+
+   public double mostrarSaldo() {
+     return saldo;
+   }
+
+   public String mostrarTitularConta() {
+     return titularConta;
+   }
+ }
