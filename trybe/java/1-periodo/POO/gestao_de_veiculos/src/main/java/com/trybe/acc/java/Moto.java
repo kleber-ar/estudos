d423069fabe20a40ebee5bb15190e7a2e3cf9199
kleber-ar <kleber-ar@hotmail.com>
2026-06-24 06:22:29 -0300

    java POO gestao de veiculos: REQ3 add class moto

--- trybe/java/1-periodo/POO/gestao_de_veiculos/src/main/java/com/trybe/acc/java/Moto.java
+++ trybe/java/1-periodo/POO/gestao_de_veiculos/src/main/java/com/trybe/acc/java/Moto.java
@@ -1,13 +1,34 @@
-package com.trybe.acc.java;
-
-/**
- * Método principal.
- */
-
 public class Moto extends VeiculoBase {
 
-  /**
-   * Atributos e Métodos.
-   */
+  private String tipoMoto;
+
+  public Moto(String marca, String modelo, int ano, String tipoCombustivel, int capacidadeTanque,
+      String tipoMoto) {
+    super(marca, modelo, ano, tipoCombustivel, capacidadeTanque);
+    this.tipoMoto = tipoMoto;
+  }
+
+  public double calcularConsumoCombustivel(double distancia) {
+    double taxaConsumo = 18.0; // Taxa de consumo em km por litro para uma moto
+    double consumoCombustivel = distancia / taxaConsumo;
+    return consumoCombustivel;
+  }
+
+  public void exibirInformacoes() {
+    System.out.println("Marca: " + getMarca());
+    System.out.println("Modelo: " + getModelo());
+    System.out.println("Ano: " + getAno());
+    System.out.println("Tipo de combustível: " + getTipoCombustivel());
+    System.out.println("Capacidade do tanque: " + getCapacidadeTanque() + " litros");
+    System.out.println("Tipo da moto: " + getTipoMoto());
+  }
+
+  public String getTipoMoto() {
+    return tipoMoto;
+  }
+
+  public void setTipoMoto(String tipoMoto) {
+    this.tipoMoto = tipoMoto;
+  }
 
 }
