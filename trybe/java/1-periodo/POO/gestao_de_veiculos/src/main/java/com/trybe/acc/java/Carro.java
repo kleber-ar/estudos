cb955b8356b8066e0001296f91715afdc055b10e
kleber-ar <kleber-ar@hotmail.com>
2026-06-24 06:20:53 -0300

    java POO gestao de veiculos: REQ1 add class carro

--- trybe/java/1-periodo/POO/gestao_de_veiculos/src/main/java/com/trybe/acc/java/Carro.java
+++ trybe/java/1-periodo/POO/gestao_de_veiculos/src/main/java/com/trybe/acc/java/Carro.java
@@ -1,13 +1,33 @@
-package com.trybe.acc.java;
-
-/**
- * Método principal.
- */
-
 public class Carro extends VeiculoBase {
 
-  /**
-   * Atributos e Métodos.
-   */
+  private int numPassageiros;
 
+  public Carro(String marca, String modelo, int ano, String tipoCombustivel, int capacidadeTanque,
+      int numPassageiros) {
+    super(marca, modelo, ano, tipoCombustivel, capacidadeTanque);
+    this.numPassageiros = numPassageiros;
+  }
+
+  public double calcularConsumoCombustivel(double distancia) {
+    double taxaConsumo = 10.0; // Taxa de consumo em km por litro para um carro
+    double consumoCombustivel = distancia / taxaConsumo;
+    return consumoCombustivel;
+  }
+
+  public void exibirInformacoes() {
+    System.out.println("Marca: " + getMarca());
+    System.out.println("Modelo: " + getModelo());
+    System.out.println("Ano: " + getAno());
+    System.out.println("Tipo de combustível: " + getTipoCombustivel());
+    System.out.println("Capacidade do tanque: " + getCapacidadeTanque() + " litros");
+    System.out.println("Número de passageiros: " + getNumPassageiros());
+  }
+
+  public int getNumPassageiros() {
+    return numPassageiros;
+  }
+
+  public void setNumPassageiros(int numPassageiros) {
+    this.numPassageiros = numPassageiros;
+  }
 }
