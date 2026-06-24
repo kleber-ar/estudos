71898a131e2fbefa338229b80820a4db39420ea8
kleber-ar <kleber-ar@hotmail.com>
2026-06-24 06:21:45 -0300

    java POO gestao de veiculos: REQ2 add class caminhão

--- trybe/java/1-periodo/POO/gestao_de_veiculos/src/main/java/com/trybe/acc/java/Caminhao.java
+++ trybe/java/1-periodo/POO/gestao_de_veiculos/src/main/java/com/trybe/acc/java/Caminhao.java
@@ -1,13 +1,34 @@
-package com.trybe.acc.java;
-
-/**
- * Método principal.
- */
-
 public class Caminhao extends VeiculoBase {
 
-  /**
-   * Atributos e Métodos.
-   */
+  private double capacidadeCarga;
+
+  public Caminhao(String marca, String modelo, int ano, String tipoCombustivel,
+      int capacidadeTanque, double capacidadeCarga) {
+    super(marca, modelo, ano, tipoCombustivel, capacidadeTanque);
+    this.capacidadeCarga = capacidadeCarga;
+  }
+
+  public double calcularConsumoCombustivel(double distancia) {
+    double taxaConsumo = 6.0; // Taxa de consumo em km por litro para um caminhão
+    double consumoCombustivel = distancia / taxaConsumo;
+    return consumoCombustivel;
+  }
+
+  public void exibirInformacoes() {
+    System.out.println("Marca: " + getMarca());
+    System.out.println("Modelo: " + getModelo());
+    System.out.println("Ano: " + getAno());
+    System.out.println("Tipo de combustível: " + getTipoCombustivel());
+    System.out.println("Capacidade do tanque: " + getCapacidadeTanque() + " litros");
+    System.out.println("Capacidade de carga: " + getCapacidadeCarga() + " kg");
+  }
+
+  public double getCapacidadeCarga() {
+    return capacidadeCarga;
+  }
+
+  public void setCapacidadeCarga(double capacidadeCarga) {
+    this.capacidadeCarga = capacidadeCarga;
+  }
 
 }
