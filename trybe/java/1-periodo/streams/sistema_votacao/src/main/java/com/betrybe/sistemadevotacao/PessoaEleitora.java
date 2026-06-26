85644d264177dc9a0208e303a2a3d23a5d7b22fa
kleber-ar <kleber-ar@hotmail.com>
2026-06-26 17:49:15 -0300

    sistema de votacao: REQ3 add class PessoaEleitora

--- trybe/java/1-periodo/streams/sistema_votacao/src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ trybe/java/1-periodo/streams/sistema_votacao/src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,22 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaEleitora{
+/**
+ * Representa uma pessoa eleitora.
+ */
+public class PessoaEleitora extends Pessoa {
 
+  private String cpf;
+
+  public PessoaEleitora(String nome, String cpf) {
+    this.nome = nome;
+    this.cpf = cpf;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
