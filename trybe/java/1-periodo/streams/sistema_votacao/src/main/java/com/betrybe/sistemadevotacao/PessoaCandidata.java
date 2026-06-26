9db3b6674659870af8975a9ca16358f579e6f378
kleber-ar <kleber-ar@hotmail.com>
2026-06-26 17:33:51 -0300

    sistema de votacao: REQ2 add class PessoaCandidata

--- trybe/java/1-periodo/streams/sistema_votacao/src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ trybe/java/1-periodo/streams/sistema_votacao/src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,32 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+/**
+ * Representa uma pessoa candidata.
+ */
+public class PessoaCandidata extends Pessoa {
 
+  private int numero;
+  private int votos;
+
+  public PessoaCandidata(String nome, int numero) {
+    this.nome = nome;
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void receberVoto() {
+    votos += 1;
+  }
 }
