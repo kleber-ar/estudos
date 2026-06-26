bf9b3b256fa393b19f6e962bec1541c77cd897cd
kleber-ar <kleber-ar@hotmail.com>
2026-06-26 18:31:41 -0300

    sistema de votacao: REQ5 completed class GerenciamentoVotaçao

--- trybe/java/1-periodo/streams/sistema_votacao/src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ trybe/java/1-periodo/streams/sistema_votacao/src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -11,4 +11,33 @@ public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
   private ArrayList<PessoaEleitora> pessoasEleitoras;
   private ArrayList<String> cpfsComputados;
 
+  public GerenciamentoVotacao() {
+    this.pessoasCandidatas = new ArrayList<>();
+    this.pessoasEleitoras = new ArrayList<>();
+    this.cpfsComputados = new ArrayList<>();
+  }
+
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+    for (PessoaCandidata candidata : pessoasCandidatas) {
+      if (candidata.getNumero() == numero) {
+        System.out.println("Número da pessoa candidata já utilizado!");
+        return;
+      }
+    }
+
+    PessoaCandidata novaCandidata = new PessoaCandidata(nome, numero);
+    pessoasCandidatas.add(novaCandidata);
+  }
+
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+    for (PessoaEleitora eleitora : pessoasEleitoras) {
+      if (eleitora.getCpf().equals(cpf)) {
+        System.out.println("Pessoa eleitora já cadastrada!");
+        return;
+      }
+    }
+
+    PessoaEleitora novaEleitora = new PessoaEleitora(nome, cpf);
+    pessoasEleitoras.add(novaEleitora);
+  }
 }
