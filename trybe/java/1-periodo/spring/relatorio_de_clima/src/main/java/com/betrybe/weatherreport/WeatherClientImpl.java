2dba992b6808b14c777898a566329a26c1abe18e
kleber-ar <kleber-ar@hotmail.com>
2026-07-02 11:42:25 -0300

    relatorio de clima: REQ1 - add weatherImpl and bean weatherConfig

---
+++ trybe/java/1-periodo/spring/relatorio_de_clima/src/main/java/com/betrybe/weatherreport/WeatherClientImpl.java
@@ -0,0 +1,10 @@
+package com.betrybe.weatherreport.solution;
+
+import com.betrybe.weatherreport.interfaces.WeatherClient;
+
+public class WeatherClientImpl implements WeatherClient {
+  @Override
+  public String getWeather(String city) {
+    return "tempinho bom";
+  }
+}
