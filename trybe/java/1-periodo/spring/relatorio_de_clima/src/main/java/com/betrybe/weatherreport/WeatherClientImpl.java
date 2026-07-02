8b12374f0a29bf294f9a7c9587a7490c73cdd8d3
kleber-ar <kleber-ar@hotmail.com>
2026-07-02 11:52:27 -0300

    relatorio de clima: REQ1 - add coments javadoc

--- trybe/java/1-periodo/spring/relatorio_de_clima/src/main/java/com/betrybe/weatherreport/WeatherClientImpl.java
+++ trybe/java/1-periodo/spring/relatorio_de_clima/src/main/java/com/betrybe/weatherreport/WeatherClientImpl.java
@@ -2,6 +2,9 @@ package com.betrybe.weatherreport.solution;
 
 import com.betrybe.weatherreport.interfaces.WeatherClient;
 
+/**
+ * retorna o tempo.
+ */
 public class WeatherClientImpl implements WeatherClient {
   @Override
   public String getWeather(String city) {
