bc6019e6f771f71f1f188f75bad30b382fbb0bb4
kleber-ar <kleber-ar@hotmail.com>
2026-07-03 16:55:13 -0300

    relatorio de clima: REQ2 - add weatherService

---
+++ trybe/java/1-periodo/spring/relatorio_de_clima/src/main/java/com/betrybe/weatherreport/WeatherServiceImpl.java
@@ -0,0 +1,27 @@
+package com.betrybe.weatherreport.solution;
+
+import com.betrybe.weatherreport.interfaces.WeatherClient;
+import com.betrybe.weatherreport.interfaces.WeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of WeatherService component.
+ */
+@Component
+public class WeatherServiceImpl implements WeatherService {
+  
+  private WeatherClient weatherClient;
+
+  @Autowired
+  public WeatherServiceImpl(WeatherClient weatherClient) {
+    this.weatherClient = weatherClient;
+  }
+
+  @Override
+  public String getWeatherReport(String city) {
+    String weather = weatherClient.getWeather(city);
+    
+    return String.format("O clima é: %s", weather);
+  }
+}
