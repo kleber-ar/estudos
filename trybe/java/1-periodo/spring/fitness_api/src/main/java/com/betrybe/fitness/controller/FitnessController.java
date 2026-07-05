995b5e939fec6fa1eb15eda0660904aad13c3751
kleber-ar <kleber-ar@hotmail.com>
2026-07-05 15:21:08 -0300

    fitness api: REQ4 add GET/fitness

--- trybe/java/1-periodo/spring/fitness_api/src/main/java/com/betrybe/fitness/controller/FitnessController.java
+++ trybe/java/1-periodo/spring/fitness_api/src/main/java/com/betrybe/fitness/controller/FitnessController.java
@@ -3,6 +3,7 @@ package com.betrybe.fitness.controller;
 
 import com.betrybe.fitness.service.FitnessServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +18,10 @@ public class FitnessController implements FitnessControllerInterface {
   @Autowired
   public FitnessController(FitnessServiceInterface service) {
     this.service = service;
+  }
+
+  @GetMapping
+  public String getRoot() {
+    return "Boas vindas à API de Fitness!";
   }
 }
