b9d1eaa40e93f899cc07cdcfae22303d3f7dea00
kleber-ar <kleber-ar@hotmail.com>
2026-07-05 16:26:52 -0300

    fitness api: REQ8 add POST /fitness/workouts controller

--- trybe/java/1-periodo/spring/fitness_api/src/main/java/com/betrybe/fitness/controller/FitnessController.java
+++ trybe/java/1-periodo/spring/fitness_api/src/main/java/com/betrybe/fitness/controller/FitnessController.java
@@ -1,13 +1,17 @@
 
 package com.betrybe.fitness.controller;
 
+import com.betrybe.fitness.dto.WorkoutCreationDto;
 import com.betrybe.fitness.dto.WorkoutDto;
 import com.betrybe.fitness.service.FitnessServiceInterface;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,9 +33,15 @@ public class FitnessController implements FitnessControllerInterface {
     return "Boas vindas à API de Fitness!";
   }
 
+  @PostMapping("/workouts")
+  public ResponseEntity<WorkoutDto> createWorkout(@RequestBody WorkoutCreationDto newWorkoutDto) {
+    return ResponseEntity.status(HttpStatus.CREATED)
+        .body(service.saveWorkout(newWorkoutDto));
+  }
+
   /**
    * Workout por id.
-   */ 
+   */
   @GetMapping("/workouts/{id}")
   public ResponseEntity<WorkoutDto> getWorkout(@PathVariable Long id) {
     // Consultamos a camada de serviço e 
