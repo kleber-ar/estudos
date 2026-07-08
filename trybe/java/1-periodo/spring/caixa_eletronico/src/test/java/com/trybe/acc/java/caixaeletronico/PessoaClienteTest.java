1007339dc1bad43097cbcc16b7e6c533755f696d
kleber-ar <kleber-ar@hotmail.com>
2026-07-08 12:44:16 -0300

    caixa eletronico: add tests pessoaClienteTest

--- trybe/java/1-periodo/spring/caixa_eletronico/src/test/java/com/trybe/acc/java/caixaeletronico/PessoaClienteTest.java
+++ trybe/java/1-periodo/spring/caixa_eletronico/src/test/java/com/trybe/acc/java/caixaeletronico/PessoaClienteTest.java
@@ -1,74 +1,140 @@
 package com.trybe.acc.java.caixaeletronico;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Teste dos métodos da classe PessoaCliente.
+ *
+ */
 @DisplayName("Teste dos métodos da classe PessoaCliente")
 class PessoaClienteTest {
 
   @Test
   @DisplayName("11 - Testa o construtor da classe Pessoa Cliente.")
   void construtorTest() {
-    fail("Não implementado");
+    PessoaCliente pessoaCliente = new PessoaCliente("Camaragibe Silva", "433.892.200-11", "1234");
 
+    assertEquals("433.892.200-11", pessoaCliente.getCpf());
   }
 
   @Test
   @DisplayName("12 - Testa o método adicionar conta e o método retornar número de contas.")
   void adicionarContaTestRetornaNumeroDeContasTest() {
-    fail("Não implementado");
+    Banco banco = new Banco();
+    PessoaCliente pessoaCliente = new PessoaCliente("Camaragibe Silva", "433.892.200-11", "1234");
+    Conta conta = new Conta("Poupança", pessoaCliente, banco);
+    pessoaCliente.adicionarConta(conta);
 
+    assertEquals(1, pessoaCliente.retornaNumeroDeContas());
   }
 
   @Test
   @DisplayName("13 - Testa o método retornar saldo de uma conta específica da pessoa cliente.")
   void retornarSaldoContaEspecificaTest() {
-    fail("Não implementado");
+    Banco banco = new Banco();
+    PessoaCliente pessoaCliente = new PessoaCliente("Camaragibe Silva", "433.892.200-11", "1234");
+    Conta conta = new Conta("Poupança", pessoaCliente, banco);
+    pessoaCliente.adicionarConta(conta);
+    conta.adicionarTransacao(2000, "Depósito recebido");
 
+    assertEquals(2000, pessoaCliente.retornarSaldoContaEspecifica(0));
   }
 
 
   @Test
   @DisplayName("14 - Testa o método retornar id de uma conta específica da pessoa cliente.")
   void retornarIdContaEspecificaTest() {
-    fail("Não implementado");
+    Banco banco = new Banco();
+    PessoaCliente pessoaCliente = new PessoaCliente("Camaragibe Silva", "433.892.200-11", "1234");
+    Conta conta = new Conta("Poupança", pessoaCliente, banco);
+    pessoaCliente.adicionarConta(conta);
 
+    assertNotEquals(null, pessoaCliente.retornarIdContaEspecifica(0));
   }
 
   @Test
   @DisplayName("15 - Testa o método retornar o extrato de uma conta específica da pessoa cliente.")
   void retornarExtratoContaEspecificaTest() {
-    fail("Não implementado");
+    Banco banco = new Banco();
+    PessoaCliente pessoaCliente = new PessoaCliente("Camaragibe Silva", "433.892.200-11", "1234");
+    Conta conta = new Conta("Poupança", pessoaCliente, banco);
+    pessoaCliente.adicionarConta(conta);
 
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    PrintStream printStream = new PrintStream(baos);
+    System.setOut(printStream);
+
+    pessoaCliente.retornarExtratoContaEspecifica(0);
+
+    String[] linha = baos.toString().split(System.lineSeparator());
+    String saida = String.join(",", linha);
+
+    assertNotEquals(null, saida);
   }
 
   @Test
   @DisplayName("16 - Testa o método adiciona transação de uma conta específica da pessoa cliente.")
   void adicionarTransacaoContaEspecificaTest() {
-    fail("Não implementado");
+    Banco banco = new Banco();
+    PessoaCliente pessoaCliente = new PessoaCliente("Camaragibe Silva", "433.892.200-11", "1234");
+    Conta conta = new Conta("Poupança", pessoaCliente, banco);
+    pessoaCliente.adicionarConta(conta);
 
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    PrintStream printStream = new PrintStream(baos);
+    System.setOut(printStream);
+
+    pessoaCliente.adicionarTransacaoContaEspecifica(0, 2000, "Depósito recebido");
+    pessoaCliente.retornarExtratoContaEspecifica(0);
+
+    String[] linha = baos.toString().split(System.lineSeparator());
+    String saida = String.join(",", linha);
+
+    assertTrue(saida.contains("2000"));
   }
 
   @Test
   @DisplayName("17 - Testa o método validar senha.")
   void validarSenhaTest() {
-    fail("Não implementado");
+    PessoaCliente pessoaCliente = new PessoaCliente("Camaragibe Silva", "433.892.200-11", "1234");
 
+    assertTrue(pessoaCliente.validarSenha("1234"));
+    assertFalse(pessoaCliente.validarSenha("12345"));
   }
 
   @Test
   @DisplayName("18 - Testa o método retornar resumo contas.")
   void retornarResumoContasTest() {
-    fail("Não implementado");
+    Banco banco = new Banco();
+    PessoaCliente pessoaCliente = new PessoaCliente("Camaragibe Silva", "433.892.200-11", "1234");
+    Conta conta = new Conta("Poupança", pessoaCliente, banco);
+    pessoaCliente.adicionarConta(conta);
 
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    PrintStream printStream = new PrintStream(baos);
+    System.setOut(printStream);
+
+    pessoaCliente.retornarResumoContas();
+
+    String[] linha = baos.toString().split(System.lineSeparator());
+    String saida = String.join(",", linha);
+
+    assertTrue(saida.contains("0"));
 
   }
 
   @Test
   @DisplayName("19 - Testa o método Getter do atributo cpf está retornando.")
   void getCpfTest() {
-    fail("Não implementado");
+    PessoaCliente pessoaCliente = new PessoaCliente("Camaragibe Silva", "433.892.200-11", "1234");
+    assertEquals("433.892.200-11", pessoaCliente.getCpf());
 
   }
 
