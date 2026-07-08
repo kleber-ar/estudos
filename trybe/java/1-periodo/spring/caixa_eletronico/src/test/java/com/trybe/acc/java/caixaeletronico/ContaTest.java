14e3a1151c828b64d0d9c4b20ca560e19f92dfab
kleber-ar <kleber-ar@hotmail.com>
2026-07-08 12:42:43 -0300

    caixa eletronico: add tests contaTest

--- trybe/java/1-periodo/spring/caixa_eletronico/src/test/java/com/trybe/acc/java/caixaeletronico/ContaTest.java
+++ trybe/java/1-periodo/spring/caixa_eletronico/src/test/java/com/trybe/acc/java/caixaeletronico/ContaTest.java
@@ -1,6 +1,13 @@
 package com.trybe.acc.java.caixaeletronico;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,41 +17,92 @@ class ContaTest {
   @Test
   @DisplayName("5 - Testa o construtor da classe conta.")
   void construtorTest() {
-    fail("Não implementado");
+    String cpf = "123.456.789-00";
+    String nome = "Fulano";
+    String senha = "password";
+
+    assertDoesNotThrow(() -> {
+      PessoaCliente cliente = new PessoaCliente(nome, cpf, senha);
+      new Conta("Corrente", cliente, new Banco());
+    });
   }
 
   @Test
   @DisplayName("6 - Testa o método adicionar transação e retornar saldo da conta.")
   void adicionarTransacaoTestRetornarSaldoTest() {
-    fail("Não implementado");
+    String cpf = "123.456.789-00";
+    String nome = "Fulano";
+    String senha = "password";
 
+    PessoaCliente cliente = new PessoaCliente(nome, cpf, senha);
+    Conta conta = new Conta("Corrente", cliente, new Banco());
+    conta.adicionarTransacao(500.0, "Transação Teste");
+
+    assertEquals(500.0, conta.retornarSaldo());
   }
 
   @Test
   @DisplayName("7 - Testa o método retornar resumo está retornando uma string com os valores corretamente.")
   void retornarResumoContaTest() {
-    fail("Não implementado");
+    String cpf = "123.456.789-00";
+    String nome = "Fulano";
+    String senha = "password";
 
+    PessoaCliente cliente = new PessoaCliente(nome, cpf, senha);
+    Conta conta = new Conta("Corrente", cliente, new Banco());
+    String resumo = conta.retornarResumoConta();
+    String resumoEsperado = conta.getIdConta() + " : R$0.00 : Corrente";
+    assertEquals(resumoEsperado, resumo);
   }
 
   @Test
   @DisplayName("8 - Testa o método retornar extrato está imprimindo os valores corretamente.")
   void retornarExtratoTest() {
-    fail("Não implementado");
+    String cpf = "123.456.789-00";
+    String nome = "Fulano";
+    String senha = "password";
+    PessoaCliente cliente = new PessoaCliente(nome, cpf, senha);
+    Conta conta = new Conta("Corrente", cliente, new Banco());
+    conta.adicionarTransacao(500.0, "Transação Teste");
 
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    PrintStream printStream = new PrintStream(outputStream);
+    System.setOut(printStream);
+
+    conta.retornarExtrato();
+
+    String output = outputStream.toString();
+    String expectedDate =
+        LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+
+    assertTrue(output.contains(expectedDate));
+
+    System.setOut(System.out);
   }
+
 
   @Test
   @DisplayName("9 - Testa o método Getter do atributo idConta está retornando.")
   void getIdContaTest() {
-    fail("Não implementado");
 
+    String cpf = "123.456.789-00";
+    String nome = "Fulano";
+    String senha = "password";
+
+    PessoaCliente cliente = new PessoaCliente(nome, cpf, senha);
+    Conta conta = new Conta("Corrente", cliente, new Banco());
+    assertEquals(10, conta.getIdConta().length());
   }
 
   @Test
   @DisplayName("10 - Testa o método método Getter do atributo pessoaCliente está retornando.")
   void getPessoaClienteTest() {
-    fail("Não implementado");
-  }
+    String cpf = "123.456.789-00";
+    String nome = "Fulano";
+    String senha = "password";
 
+    PessoaCliente cliente = new PessoaCliente(nome, cpf, senha);
+    Conta conta = new Conta("Corrente", cliente, new Banco());
+    assertSame(cliente, conta.getPessoaCliente());
+  }
 }
