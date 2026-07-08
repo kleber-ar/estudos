2fcb71615b5441d57dac409cbb89523cfb125e08
kleber-ar <kleber-ar@hotmail.com>
2026-07-08 12:41:27 -0300

    caixa eletronico: add tests transação

--- trybe/java/1-periodo/spring/caixa_eletronico/src/test/java/com/trybe/acc/java/caixaeletronico/TransacaoTest.java
+++ trybe/java/1-periodo/spring/caixa_eletronico/src/test/java/com/trybe/acc/java/caixaeletronico/TransacaoTest.java
@@ -1,40 +1,49 @@
 package com.trybe.acc.java.caixaeletronico;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("Testes dos métodos da classe Transação")
 class TransacaoTest {
 
-
   @Test
   @DisplayName("1 - Testa o método construtor da classe Transação.")
   void construtorTest() {
-    fail("Não implementado");
-
+    Transacao transacao = new Transacao(2000, "Depósito recebido");
+    assertEquals(2000, transacao.getQuantia());
   }
 
 
   @Test
   @DisplayName("2 - Testa o método Getter do atributo quantia.")
   void getQuantiaTest() {
-    fail("Não implementado");
-
+    Transacao transacao = new Transacao(2000, "Depósito recebido");
+    assertEquals(2000, transacao.getQuantia());
   }
 
   @Test
   @DisplayName("3 - Testa o método retornar resumo transação.")
   void retornarResumoTransacaoTest() {
-    fail("Não implementado");
-
+    Transacao transacao = new Transacao(2000, "Depósito recebido");
+    assertNotEquals(null, transacao.retornarResumoTransacao());
   }
 
   @Test
   @DisplayName("4 - Testa o método instante está gerando o instante corretamente.")
   void retornarInstanteTest() {
-    fail("Não implementado");
+    Transacao transacao = new Transacao(2000, "Depósito recebido");
 
+    String formatoInstante = "dd/MM/yyyy HH:mm:ss";
+    DateTimeFormatter formatadorInstante = DateTimeFormatter.ofPattern(formatoInstante);
+    LocalDateTime instanteAux = LocalDateTime.now();
+
+    assertTrue(
+        transacao.retornarResumoTransacao().contains(formatadorInstante.format(instanteAux)));
   }
 
 }
