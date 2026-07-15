c91a4dfe2be852b547ef0916dbaf4213c81af04a
kleber-ar <kleber-ar@hotmail.com>
2026-07-15 06:10:15 -0300

    products: REQ2 config notation logs conections

--- trybe/java/2-periodo/products/src/main/java/com/app/products/ProductsApplication.java
+++ trybe/java/2-periodo/products/src/main/java/com/app/products/ProductsApplication.java
@@ -2,12 +2,18 @@ package com.app.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.app.products.models.entities")
+@EnableJpaRepositories("com.app.products.models.repositories")
+@ComponentScan("com.app.products")
 public class ProductsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductsApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ProductsApplication.class, args);
+  }
 
 }
