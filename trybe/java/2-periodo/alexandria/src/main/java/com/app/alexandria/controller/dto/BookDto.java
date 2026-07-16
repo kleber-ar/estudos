cf117bd0eb938dbb0b2a687760422049118d8943
kleber-ar <kleber-ar@hotmail.com>
2026-07-16 05:40:32 -0300

    alexandria: REQ16 add  1:N bookDto

--- trybe/java/2-periodo/alexandria/src/main/java/com/app/alexandria/controller/dto/BookDto.java
+++ trybe/java/2-periodo/alexandria/src/main/java/com/app/alexandria/controller/dto/BookDto.java
@@ -2,12 +2,20 @@ package com.app.alexandria.controller.dto;
 
 import com.app.alexandria.entity.Book;
 
-public record BookDto(Long id, String title, String genre) {
+public record BookDto(
+    Long id,
+    String title,
+    String genre,
+    PublisherDto publisher) {
 
   public static BookDto fromEntity(Book book) {
+    PublisherDto publisherDto = book.getPublisher() != null ? PublisherDto.fromEntity(book.getPublisher()) : null;
+
     return new BookDto(
         book.getId(),
         book.getTitle(),
-        book.getGenre());
+        book.getGenre(),
+        publisherDto);
   }
+
 }
