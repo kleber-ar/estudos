2df1c9bdee26475b4d5c6627bf0a504d257f74fb
kleber-ar <kleber-ar@hotmail.com>
2026-07-17 11:04:39 -0300

    alexandria: REQ19 att bookDTO M:M

--- trybe/java/2-periodo/alexandria/src/main/java/com/app/alexandria/controller/dto/BookDto.java
+++ trybe/java/2-periodo/alexandria/src/main/java/com/app/alexandria/controller/dto/BookDto.java
@@ -1,12 +1,15 @@
 package com.app.alexandria.controller.dto;
 
+import java.util.List;
+
 import com.app.alexandria.entity.Book;
 
 public record BookDto(
     Long id,
     String title,
     String genre,
-    PublisherDto publisher) {
+    PublisherDto publisher,
+    List<AuthorDto> authors) {
 
   public static BookDto fromEntity(Book book) {
     PublisherDto publisherDto = book.getPublisher() != null ? PublisherDto.fromEntity(book.getPublisher()) : null;
@@ -15,7 +18,10 @@ public record BookDto(
         book.getId(),
         book.getTitle(),
         book.getGenre(),
-        publisherDto);
+        publisherDto,
+        book.getAuthors().stream()
+            .map(AuthorDto::fromEntity)
+            .toList());
   }
 
 }
