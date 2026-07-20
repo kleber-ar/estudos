693b7d0fc0b1272bae0e4d4579b94d442bcc3e8b
kleber-ar <kleber-ar@hotmail.com>
2026-07-20 18:28:48 -0300

    ecommerce: add dependenci h2, config profiles in properties and tests

--- trybe/java/2-periodo/java-example-ecommerce/src/test/java/com/betrybe/ecommerce/ProductServiceTest.java
+++ trybe/java/2-periodo/java-example-ecommerce/src/test/java/com/betrybe/ecommerce/ProductServiceTest.java
@@ -15,8 +15,13 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+/**
+ * Poderia usar mais de um profile por exemplo: @ActiveProfiles({"dev", "test"})
+ **/
+@ActiveProfiles("dev")
 public class ProductServiceTest {
 
   @Autowired
