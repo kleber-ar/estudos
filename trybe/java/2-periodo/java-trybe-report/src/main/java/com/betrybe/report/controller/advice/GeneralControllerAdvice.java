a5d51ba8cc7503b2b501ffa2d2c1dfca88e5262e
kleber-ar <kleber-ar@hotmail.com>
2026-07-23 20:13:40 -0300

    report: REQ4 add validation in entity and config advice controller

--- trybe/java/2-periodo/java-trybe-report/src/main/java/com/betrybe/report/controller/advice/GeneralControllerAdvice.java
+++ trybe/java/2-periodo/java-trybe-report/src/main/java/com/betrybe/report/controller/advice/GeneralControllerAdvice.java
@@ -5,11 +5,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 
 @ControllerAdvice
 public class GeneralControllerAdvice {
-  @ExceptionHandler(NotFoundException.class)
-  public ResponseEntity<String> handleNotFound(NotFoundException e) {
-    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+  @ExceptionHandler(ConstraintViolationException.class)
+  public ResponseEntity<Map<String, String>> handleConstraintViolation(ConstraintViolationException e) {
+    Map<String, String> errors = new HashMap<>();
+    for (ConstraintViolation<?> violation : e.getConstraintViolations()) {
+      errors.put(violation.getPropertyPath().toString(), violation.getMessage());
+    }
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
   }
 }
--- trybe/java/2-periodo/java-trybe-report/src/main/java/com/betrybe/report/entity/Product.java
+++ trybe/java/2-periodo/java-trybe-report/src/main/java/com/betrybe/report/entity/Product.java
@@ -1,10 +1,17 @@
 package com.betrybe.report.entity;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import java.time.LocalDate;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,10 +21,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Product {
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @NotBlank(message = "Name is Required")
   private String name;
+
+  @NotNull(message = "Manufacture date is Required")
+  @PastOrPresent(message = "Manufacture date must be in the Past or Present")
   private LocalDate manufactureDate;
+
+  @NotNull(message = "Expiration date is Required")
+  @Future(message = "Expiration date must be in the Future")
   private LocalDate expirationDate;
 }
