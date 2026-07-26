42fc92cfc14343a5e57aaaa05f2040097a2a10e4
kleber-ar <kleber-ar@hotmail.com>
2026-07-26 00:01:38 -0300

    webinars: att person entity and respository

--- trybe/java/2-periodo/java-trybe-webinars/src/main/java/com/betrybe/webinar/entity/Person.java
+++ trybe/java/2-periodo/java-trybe-webinars/src/main/java/com/betrybe/webinar/entity/Person.java
@@ -6,9 +6,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 @Entity
 @Table(name = "persons")
-public class Person {
+public class Person implements UserDetails {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,13 +20,19 @@ public class Person {
   private String fullname;
   private String email;
 
+  @Column(unique = true)
+  private String username;
+  private String password;
+
   public Person() {
   }
 
-  public Person(Long id, String fullname, String email) {
+  public Person(Long id, String fullname, String email, String username, String password) {
     this.id = id;
     this.fullname = fullname;
     this.email = email;
+    this.username = username;
+    this.password = password;
   }
 
   public Long getId() {
@@ -48,5 +57,48 @@ public class Person {
 
   public void setEmail(String email) {
     this.email = email;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of();
+  }
+
+  @Override
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  @Override
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
   }
 }
--- trybe/java/2-periodo/java-trybe-webinars/src/main/java/com/betrybe/webinar/repository/PersonRepository.java
+++ trybe/java/2-periodo/java-trybe-webinars/src/main/java/com/betrybe/webinar/repository/PersonRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
-
+  Optional<Person> findByUsername(String username);
 }
