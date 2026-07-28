1ecc47ba6a98f6a4bc4c973cadb08a0d2e04680b
kleber-ar <kleber-ar@hotmail.com>
2026-07-28 07:14:21 -0300

    playlist: add JwtFilter

--- trybe/java/2-periodo/java-playlist/src/main/java/com/betrybe/playlist/security/JwtFilter.java
+++ trybe/java/2-periodo/java-playlist/src/main/java/com/betrybe/playlist/security/JwtFilter.java
@@ -3,6 +3,43 @@ package com.betrybe.playlist.security;
 import org.springframework.stereotype.Component;
 
 @Component
-public class JwtFilter {
+public class JwtFilter extends OncePerRequestFilter {
 
+  private final TokenService tokenService;
+  private final PersonService personService;
+
+  @Autowired
+  public JwtFilter(TokenService tokenService, PersonService personService) {
+    this.tokenService = tokenService;
+    this.personService = personService;
+  }
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+      FilterChain filterChain) throws ServletException, IOException {
+    Optional<String> token = extractToken(request);
+
+    if (token.isPresent()) {
+      String subject = tokenService.validateToken(token.get());
+      UserDetails userDetails = personService.loadUserByUsername(subject);
+
+      UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+          userDetails, null, userDetails.getAuthorities());
+
+      SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    filterChain.doFilter(request, response);
+  }
+
+  private Optional<String> extractToken(HttpServletRequest request) {
+    String authHeader = request.getHeader("Authorization");
+
+    if (authHeader == null) {
+      return Optional.empty();
+    }
+
+    return Optional.of(
+        authHeader.replace("Bearer ", ""));
+  }
 }
