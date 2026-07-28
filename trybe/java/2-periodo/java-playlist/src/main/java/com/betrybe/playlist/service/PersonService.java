f45e5ecff50e4cadcd77c68cb4db6285b2f5ed8b
kleber-ar <kleber-ar@hotmail.com>
2026-07-27 22:51:25 -0300

    playlist: att PersonService

--- trybe/java/2-periodo/java-playlist/src/main/java/com/betrybe/playlist/service/PersonService.java
+++ trybe/java/2-periodo/java-playlist/src/main/java/com/betrybe/playlist/service/PersonService.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PersonService {
+public class PersonService implements UserDetailsService {
   private final PersonRepository personRepository;
 
   @Autowired
@@ -16,6 +16,19 @@ public class PersonService {
 
   public Person insert(Person person) {
     return personRepository.save(person);
+  }
+
+  public Person insert(Person person) {
+    // Precisamos criptografar a senha antes de armazenar a pessoa no banco.
+    String hashedPassword = new BCryptPasswordEncoder().encode(person.getPassword());
+    person.setPassword(hashedPassword);
+
+    return personRepository.save(person);
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return personRepository.findByUsername(username);
   }
 
 }
