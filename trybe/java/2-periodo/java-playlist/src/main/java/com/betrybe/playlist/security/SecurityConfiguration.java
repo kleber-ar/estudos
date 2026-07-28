0eb9b7bda37a37ad5086382eeeb7bc76b5b636bf
kleber-ar <kleber-ar@hotmail.com>
2026-07-28 07:21:19 -0300

    playlist: att SecuriyConfig rotes

--- trybe/java/2-periodo/java-playlist/src/main/java/com/betrybe/playlist/security/SecurityConfiguration.java
+++ trybe/java/2-periodo/java-playlist/src/main/java/com/betrybe/playlist/security/SecurityConfiguration.java
@@ -28,6 +28,17 @@ public class SecurityConfiguration {
         .csrf(AbstractHttpConfigurer::disable)
         .sessionManagement(
             session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll() // acesso API via Swagger
+            .requestMatchers(HttpMethod.GET, "/v3/**").permitAll() // acesso API via Swagger
+            .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+            .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+            .requestMatchers(HttpMethod.GET, "/musics").permitAll()
+            .requestMatchers(HttpMethod.POST, "/musics").permitAll()
+            .requestMatchers(HttpMethod.POST, "/playlists").permitAll()
+            .requestMatchers(HttpMethod.GET, "/playlists").permitAll()
+            .anyRequest().authenticated())
+        .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
         .build();
   }
 
