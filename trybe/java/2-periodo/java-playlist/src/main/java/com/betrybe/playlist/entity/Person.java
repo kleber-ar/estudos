9e53b0a7dc5d0a554e8e5de9da6606d9587a9a22
kleber-ar <kleber-ar@hotmail.com>
2026-07-27 22:44:20 -0300

    playlist: att Person entity

--- trybe/java/2-periodo/java-playlist/src/main/java/com/betrybe/playlist/entity/Person.java
+++ trybe/java/2-periodo/java-playlist/src/main/java/com/betrybe/playlist/entity/Person.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Person {
+public class Person implements UserDetails {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
@@ -21,5 +21,32 @@ public class Person {
   private String username;
   private String password;
   private Role role;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of();
+  }
+
+  // Em todos os métodos abaixo o retorno padrão é false e devemos alterar para
+  // true
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 
 }
