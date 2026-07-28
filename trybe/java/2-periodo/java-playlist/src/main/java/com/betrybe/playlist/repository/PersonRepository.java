09a97a77da050de34a9799c8559045ecc1601bb6
kleber-ar <kleber-ar@hotmail.com>
2026-07-27 22:47:43 -0300

    playlist: att PersonRepository

--- trybe/java/2-periodo/java-playlist/src/main/java/com/betrybe/playlist/repository/PersonRepository.java
+++ trybe/java/2-periodo/java-playlist/src/main/java/com/betrybe/playlist/repository/PersonRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
+  Person findByUsername(String username);
 }
