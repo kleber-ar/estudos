e271c7f57815e4dc56b8b3c6250cc2c77357d3cc
kleber-ar <kleber-ar@hotmail.com>
2026-07-27 22:37:26 -0300

    playlist: att SecurityConfiguration

--- trybe/java/2-periodo/java-playlist/src/main/java/com/betrybe/playlist/security/SecurityConfiguration.java
+++ trybe/java/2-periodo/java-playlist/src/main/java/com/betrybe/playlist/security/SecurityConfiguration.java
@@ -31,4 +31,15 @@ public class SecurityConfiguration {
         .build();
   }
 
+  @Bean
+  public AuthenticationManager authenticationManager(
+      AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
 }
