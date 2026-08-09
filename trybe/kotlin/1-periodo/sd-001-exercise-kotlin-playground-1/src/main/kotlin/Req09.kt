379ea4727b231e2e53f386501a311f3675df7c54
kleber-ar <kleber-ar@hotmail.com>
2026-08-09 18:34:42 -0300

    kotlin playground: REQ9 calc parcelas

--- trybe/kotlin/1-periodo/sd-001-exercise-kotlin-playground-1/src/main/kotlin/Req09.kt
+++ trybe/kotlin/1-periodo/sd-001-exercise-kotlin-playground-1/src/main/kotlin/Req09.kt
@@ -1,7 +1,32 @@
 fun calculateFinalPrice(carValue: Double, installment: Int, surcharges: Array<Double>): Double {
-    TODO("Use essa função para desenvolver sua lógica")
+  if (installment == 1) {
+    return carValue * 0.80
+  }
+
+  val surcharge =
+          when (installment) {
+            12 -> surcharges[0]
+            24 -> surcharges[1]
+            36 -> surcharges[2]
+            48 -> surcharges[3]
+            60 -> surcharges[4]
+            else -> 0.0
+          }
+
+  return carValue + (carValue * surcharge)
 }
 
 fun main() {
-    TODO("Use essa função para rodar o codigo principal")
+  val carValue = readLine()?.toDoubleOrNull()
+
+  if (carValue != null) {
+    val surcharges = arrayOf(0.06, 0.12, 0.18, 0.24, 0.30)
+
+    println("À vista: ${calculateFinalPrice(carValue, 1, surcharges)}")
+    println("12 parcelas: ${calculateFinalPrice(carValue, 12, surcharges)}")
+    println("24 parcelas: ${calculateFinalPrice(carValue, 24, surcharges)}")
+    println("36 parcelas: ${calculateFinalPrice(carValue, 36, surcharges)}")
+    println("48 parcelas: ${calculateFinalPrice(carValue, 48, surcharges)}")
+    println("60 parcelas: ${calculateFinalPrice(carValue, 60, surcharges)}")
+  }
 }
