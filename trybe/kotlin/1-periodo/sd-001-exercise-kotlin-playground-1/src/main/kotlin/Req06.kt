d6e0658d84e86bed34bfd4d5a842b52aec4c9d33
kleber-ar <kleber-ar@hotmail.com>
2026-08-09 18:06:50 -0300

    kotlin playground: REQ6 calc salary plus

--- trybe/kotlin/1-periodo/sd-001-exercise-kotlin-playground-1/src/main/kotlin/Req06.kt
+++ trybe/kotlin/1-periodo/sd-001-exercise-kotlin-playground-1/src/main/kotlin/Req06.kt
@@ -1,7 +1,18 @@
 fun calculateNewSalary(currentSalary: Double): Double {
-    TODO("Use essa função para desenvolver sua lógica")
+  return when {
+    currentSalary <= 300.0 -> currentSalary * 1.50
+    currentSalary <= 500.0 -> currentSalary * 1.40
+    currentSalary <= 700.0 -> currentSalary * 1.30
+    currentSalary <= 800.0 -> currentSalary * 1.20
+    currentSalary <= 1000.0 -> currentSalary * 1.10
+    else -> currentSalary * 1.05
+  }
 }
 
 fun main() {
-    TODO("Use essa função para rodar o codigo principal")
+  val currentSalary = readLine()?.toDoubleOrNull()
+
+  if (currentSalary != null) {
+    println(calculateNewSalary(currentSalary))
+  }
 }
