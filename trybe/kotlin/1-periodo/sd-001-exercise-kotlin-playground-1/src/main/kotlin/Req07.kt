222382decdf7b7e04328b41619b48f19c06faef5
kleber-ar <kleber-ar@hotmail.com>
2026-08-09 18:21:14 -0300

    kotlin playground: REQ7 calc peso

--- trybe/kotlin/1-periodo/sd-001-exercise-kotlin-playground-1/src/main/kotlin/Req07.kt
+++ trybe/kotlin/1-periodo/sd-001-exercise-kotlin-playground-1/src/main/kotlin/Req07.kt
@@ -1,15 +1,37 @@
 fun calculateAverage(ages: List<Int>): Double {
-    TODO("Use essa função para desenvolver sua lógica")
+  return ages.average()
 }
 
 fun countPeopleWeightHeight(weights: List<Double>, heights: List<Double>): Int {
-    TODO("Use essa função para desenvolver sua lógica")
+  var count = 0
+
+  for (i in weights.indices) {
+    if (weights[i] > 90.0 && heights[i] < 1.50) {
+      count++
+    }
+  }
+
+  return count
 }
 
 fun calculatePercentageAgeHeight(ages: List<Int>, heights: List<Double>): Int {
-    TODO("Use essa função para desenvolver sua lógica")
+  var count = 0
+
+  for (i in ages.indices) {
+    if (ages[i] in 10..30 && heights[i] > 1.90) {
+      count++
+    }
+  }
+
+  return (count * 100) / ages.size
 }
 
 fun main() {
-    TODO("Use essa função para rodar o codigo principal")
+  val ages = listOf(25, 30, 35, 40, 45)
+  val weights = listOf(80.0, 95.0, 70.0, 100.0, 98.0)
+  val heights = listOf(1.95, 1.96, 1.60, 1.96, 1.45)
+
+  println("Média das idades: ${calculateAverage(ages)}")
+  println("Peso > 90kg e altura < 1.50m: " + countPeopleWeightHeight(weights, heights))
+  println("Porcentagem: " + calculatePercentageAgeHeight(ages, heights) + "%")
 }
