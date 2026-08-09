2eceaa676314b96b6212b68e4a6c4a3ed2cc9b65
kleber-ar <kleber-ar@hotmail.com>
2026-08-09 18:01:40 -0300

    kotlin playground: REQ4 calc media

--- trybe/kotlin/1-periodo/sd-001-exercise-kotlin-playground-1/src/main/kotlin/Req04.kt
+++ trybe/kotlin/1-periodo/sd-001-exercise-kotlin-playground-1/src/main/kotlin/Req04.kt
@@ -1,19 +1,35 @@
 fun calculateAverage(number1: Double, number2: Double): Double {
-    TODO("Use essa função para desenvolver sua lógica")
+  return (number1 + number2) / 2
 }
 
 fun calculateDifference(number1: Double, number2: Double): Double {
-    TODO("Use essa função para desenvolver sua lógica")
+  return if (number1 >= number2) {
+    number1 - number2
+  } else {
+    number2 - number1
+  }
 }
 
 fun calculateProduct(number1: Double, number2: Double): Double {
-    TODO("Use essa função para desenvolver sua lógica")
+  return number1 * number2
 }
 
 fun calculateDivision(number1: Double, number2: Double): Double? {
-    TODO("Use essa função para desenvolver sua lógica")
+  if (number2 == 0.0) {
+    return null
+  }
+
+  return number1 / number2
 }
 
 fun main() {
-    TODO("Use essa função para rodar o codigo principal")
+  val number1 = readLine()?.toDoubleOrNull()
+  val number2 = readLine()?.toDoubleOrNull()
+
+  if (number1 != null && number2 != null) {
+    println("Média: ${calculateAverage(number1, number2)}")
+    println("Diferença: ${calculateDifference(number1, number2)}")
+    println("Produto: ${calculateProduct(number1, number2)}")
+    println("Divisão: ${calculateDivision(number1, number2)}")
+  }
 }
