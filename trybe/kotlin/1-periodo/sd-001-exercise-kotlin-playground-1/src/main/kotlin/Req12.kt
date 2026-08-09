cca0d5dca453bc97f592357d332cb627b2027f47
kleber-ar <kleber-ar@hotmail.com>
2026-08-09 19:35:00 -0300

    kotlin playground: REQ12 calc list mult

--- trybe/kotlin/1-periodo/sd-001-exercise-kotlin-playground-1/src/main/kotlin/Req12.kt
+++ trybe/kotlin/1-periodo/sd-001-exercise-kotlin-playground-1/src/main/kotlin/Req12.kt
@@ -1,7 +1,15 @@
 fun multiplyLists(list1: List<Int>, list2: List<Int>, result: MutableList<Int>) {
-    TODO("Use essa função para desenvolver sua lógica")
+  for (i in list1.indices) {
+    result.add(list1[i] * list2[i])
+  }
 }
 
 fun main() {
-    TODO("Use essa função para rodar o codigo principal")
+  val list1 = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  val list2 = listOf(10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
+  val result = mutableListOf<Int>()
+
+  multiplyLists(list1, list2, result)
+
+  println(result)
 }
