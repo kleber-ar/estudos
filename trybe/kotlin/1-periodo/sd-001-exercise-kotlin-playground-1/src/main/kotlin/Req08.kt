f1bdf8a510f6c6b1440febff5d9bfced82136933
kleber-ar <kleber-ar@hotmail.com>
2026-08-09 18:25:59 -0300

    kotlin playground: REQ8 calc odd

--- trybe/kotlin/1-periodo/sd-001-exercise-kotlin-playground-1/src/main/kotlin/Req08.kt
+++ trybe/kotlin/1-periodo/sd-001-exercise-kotlin-playground-1/src/main/kotlin/Req08.kt
@@ -1,11 +1,30 @@
 fun calculateSumEven(numbers: List<Int>): Int {
-    TODO("Use essa função para desenvolver sua lógica")
+  var sum = 0
+
+  for (number in numbers) {
+    if (number % 2 == 0) {
+      sum += number
+    }
+  }
+
+  return sum
 }
 
 fun calculateSumOdd(numbers: List<Int>): Int {
-    TODO("Use essa função para desenvolver sua lógica")
+  var sum = 0
+
+  for (number in numbers) {
+    if (number % 2 != 0) {
+      sum += number
+    }
+  }
+
+  return sum
 }
 
 fun main() {
-    TODO("Use essa função para rodar o codigo principal")
+  val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+  println("Soma dos pares: ${calculateSumEven(numbers)}")
+  println("Soma dos ímpares: ${calculateSumOdd(numbers)}")
 }
