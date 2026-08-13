7c64673ceb8d2c8edc374cfe07a9d1f7214032f2
kleber-ar <kleber-ar@hotmail.com>
2026-08-13 06:49:59 -0300

    login:REQ8 add regex validation email

--- trybe/kotlin/1-periodo/android-001-projeto-login-social/app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ trybe/kotlin/1-periodo/android-001-projeto-login-social/app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doAfterTextChanged
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,11 +14,21 @@ class MainActivity : AppCompatActivity() {
 
         val email = findViewById<TextInputEditText>(R.id.email_edit_text)
         val password = findViewById<TextInputEditText>(R.id.password_edit_text)
-        val loginButton = findViewById<MaterialButton>(R.id.login_button)
+
+        val emailLayout =
+            findViewById<TextInputLayout>(
+                R.id.email_text_input_layout,
+            )
+
+        val loginButton =
+            findViewById<MaterialButton>(
+                R.id.login_button,
+            )
 
         fun updateLoginButton() {
             loginButton.isEnabled =
-                !email.text.isNullOrEmpty() && !password.text.isNullOrEmpty()
+                !email.text.isNullOrEmpty() &&
+                !password.text.isNullOrEmpty()
         }
 
         email.doAfterTextChanged {
@@ -26,6 +37,17 @@ class MainActivity : AppCompatActivity() {
 
         password.doAfterTextChanged {
             updateLoginButton()
+        }
+
+        loginButton.setOnClickListener {
+            val emailRegex =
+                Regex("^[A-Za-z0-9.]+@[A-Za-z]+\\.[A-Za-z]+$")
+
+            if (!emailRegex.matches(email.text.toString())) {
+                emailLayout.error = "Email inválido"
+            } else {
+                emailLayout.error = null
+            }
         }
     }
 }
