8c6050cd801ab63d8af3e7ae67426310050411d7
kleber-ar <kleber-ar@hotmail.com>
2026-08-13 06:42:52 -0300

    login:REQ6 add enebled login button

--- trybe/kotlin/1-periodo/android-001-projeto-login-social/app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ trybe/kotlin/1-periodo/android-001-projeto-login-social/app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -2,11 +2,30 @@ package com.betrybe.sociallogin
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val email = findViewById<TextInputEditText>(R.id.email_edit_text)
+        val password = findViewById<TextInputEditText>(R.id.password_edit_text)
+        val loginButton = findViewById<MaterialButton>(R.id.login_button)
+
+        fun updateLoginButton() {
+            loginButton.isEnabled =
+                !email.text.isNullOrEmpty() && !password.text.isNullOrEmpty()
+        }
+
+        email.doAfterTextChanged {
+            updateLoginButton()
+        }
+
+        password.doAfterTextChanged {
+            updateLoginButton()
+        }
     }
 }
