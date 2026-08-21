bf108467e195d3959bfce8a13b5cb2ec5951751f
kleber-ar <kleber-ar@hotmail.com>
2026-08-21 17:02:45 -0300

    virtual menu: REQ6 add button voltar

--- trybe/kotlin/1-periodo/android-001-projeto-trybe-virtual-menu/app/src/main/java/com/example/trybevirtualmenu/views/MenuItemDetailActivity.kt
+++ trybe/kotlin/1-periodo/android-001-projeto-trybe-virtual-menu/app/src/main/java/com/example/trybevirtualmenu/views/MenuItemDetailActivity.kt
@@ -5,12 +5,14 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.trybevirtualmenu.R
+import com.google.android.material.button.MaterialButton
 
 class MenuItemDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_item_detail)
 
+        val detailBack: MaterialButton = findViewById(R.id.detail_back)
         val detailImage: ImageView = findViewById(R.id.detail_image)
         val detailName: TextView = findViewById(R.id.detail_name)
         val detailDescription: TextView = findViewById(R.id.detail_description)
@@ -25,5 +27,9 @@ class MenuItemDetailActivity : AppCompatActivity() {
         detailName.text = name
         detailDescription.text = description
         detailPrice.text = price
+
+        detailBack.setOnClickListener {
+            finish()
+        }
     }
 }
