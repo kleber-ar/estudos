e7151350d03050f3263fbccef80425621b726e59
kleber-ar <kleber-ar@hotmail.com>
2026-08-31 12:00:37 -0300

    cambio: REQ9 add GET rates

--- trybe/kotlin/2-periodo/android-001-projeto-android-cambio/app/src/main/java/com/betrybe/currencyview/data/api/ApiService.kt
+++ trybe/kotlin/2-periodo/android-001-projeto-android-cambio/app/src/main/java/com/betrybe/currencyview/data/api/ApiService.kt
@@ -1,9 +1,16 @@
 package com.betrybe.currencyview.data.api
 
+import com.betrybe.currencyview.data.models.CurrencyRateResponse
 import com.betrybe.currencyview.data.models.CurrencySymbolResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
     @GET("symbols?access_key=gKSrABlHD03DgJxz5bn3CKCC0XK4gY01")
     suspend fun getSymbols(): CurrencySymbolResponse
+
+    @GET("latest?access_key=gKSrABlHD03DgJxz5bn3CKCC0XK4gY01")
+    suspend fun getLatestRates(
+        @Query("base") base: String,
+    ): CurrencyRateResponse
 }
