ccd8cb13d48243c9b036e9acbf5a9a12aa236d21
kleber-ar <kleber-ar@hotmail.com>
2026-09-05 05:59:24 -0300

    Trybebnb: REQ3 add fragments for bottom navigation

--- trybe/kotlin/2-periodo/android-001-projeto-trybnb/app/src/main/java/com/betrybe/trybnb/ui/views/activities/MainActivity.kt
+++ trybe/kotlin/2-periodo/android-001-projeto-trybnb/app/src/main/java/com/betrybe/trybnb/ui/views/activities/MainActivity.kt
@@ -2,12 +2,42 @@ package com.betrybe.trybnb.ui.views.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.betrybe.trybnb.R
+import com.example.trybnb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(
+                    R.id.main_fragment_container,
+                    ReservationFragment(),
+                ).commit()
+        }
+
+        binding.navigationBottomView.setOnItemSelectedListener { item ->
+
+            val fragment =
+                when (item.itemId) {
+                    R.id.reservation_menu_item -> ReservationFragment()
+                    R.id.create_reservation_menu_item -> CreateReservationFragment()
+                    R.id.profile_menu_tem -> ProfileFragment()
+                    else -> return@setOnItemSelectedListener false
+                }
+
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.main_fragment_container, fragment)
+                .commit()
+
+            true
+        }
     }
 }
