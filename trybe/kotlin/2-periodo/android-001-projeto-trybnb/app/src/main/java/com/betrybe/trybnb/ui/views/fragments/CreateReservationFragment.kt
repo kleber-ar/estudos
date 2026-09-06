c6b693ad6bf4093c8c25009946b5240fdcab767a
kleber-ar <kleber-ar@hotmail.com>
2026-09-06 17:15:27 -0300

    Trybebnb: REQ10 add validations for createReservation

--- trybe/kotlin/2-periodo/android-001-projeto-trybnb/app/src/main/java/com/betrybe/trybnb/ui/views/fragments/CreateReservationFragment.kt
+++ trybe/kotlin/2-periodo/android-001-projeto-trybnb/app/src/main/java/com/betrybe/trybnb/ui/views/fragments/CreateReservationFragment.kt
@@ -25,6 +25,106 @@ class CreateReservationFragment : Fragment() {
         return binding!!.root
     }
 
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(
+            view,
+            savedInstanceState,
+        )
+
+        binding!!.createReservationButton.setOnClickListener {
+            validateFields()
+        }
+    }
+
+    private fun validateFields() {
+        val firstName =
+            binding!!
+                .firstNameCreateReservation
+                .editText
+                ?.text
+                .toString()
+
+        val lastName =
+            binding!!
+                .lastNameCreateReservation
+                .editText
+                ?.text
+                .toString()
+
+        val checkin =
+            binding!!
+                .checkinCreateReservation
+                .editText
+                ?.text
+                .toString()
+
+        val checkout =
+            binding!!
+                .checkoutCreateReservation
+                .editText
+                ?.text
+                .toString()
+
+        val additionalNeeds =
+            binding!!
+                .additionalNeedsCreateReservation
+                .editText
+                ?.text
+                .toString()
+
+        val totalPrice =
+            binding!!
+                .totalPriceCreateReservation
+                .editText
+                ?.text
+                .toString()
+
+        binding!!.firstNameCreateReservation.error =
+            if (firstName.isBlank()) {
+                "O campo Nome é obrigatório"
+            } else {
+                null
+            }
+
+        binding!!.lastNameCreateReservation.error =
+            if (lastName.isBlank()) {
+                "O campo Sobrenome é obrigatório"
+            } else {
+                null
+            }
+
+        binding!!.checkinCreateReservation.error =
+            if (checkin.isBlank()) {
+                "O campo Checkin é obrigatório"
+            } else {
+                null
+            }
+
+        binding!!.checkoutCreateReservation.error =
+            if (checkout.isBlank()) {
+                "O campo Checkout é obrigatório"
+            } else {
+                null
+            }
+
+        binding!!.additionalNeedsCreateReservation.error =
+            if (additionalNeeds.isBlank()) {
+                "O campo Necessidades Adicionais é obrigatório"
+            } else {
+                null
+            }
+
+        binding!!.totalPriceCreateReservation.error =
+            if (totalPrice.isBlank()) {
+                "O campo Preço Total é obrigatório"
+            } else {
+                null
+            }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
