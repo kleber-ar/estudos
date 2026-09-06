c75bd383c2a9aa8a076aa42fb457e5543570c60e
kleber-ar <kleber-ar@hotmail.com>
2026-09-05 23:26:19 -0300

    Trybebnb: REQ7 add fragment de reserva

---
+++ trybe/kotlin/2-periodo/android-001-projeto-trybnb/app/src/main/java/com/betrybe/trybnb/ui/views/fragments/ReservationFragment.kt
@@ -0,0 +1,32 @@
+package com.betrybe.trybnb.ui.views.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.trybnb.databinding.FragmentReservationBinding
+
+class ReservationFragment : Fragment() {
+    private var binding: FragmentReservationBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding =
+            FragmentReservationBinding.inflate(
+                inflater,
+                container,
+                false,
+            )
+
+        return binding!!.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
